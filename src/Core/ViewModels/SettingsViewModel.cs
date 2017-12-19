ab49b3596906d04e34befc93b9679d8cb601007a
Michael Fink <michael.fink@asamnet.de>
2017-12-19 08:25:12 +0100

    re-implemented settings page; Picker objects can't use Converter classes

--- src/Core/ViewModels/SettingsViewModel.cs
+++ src/Core/ViewModels/SettingsViewModel.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.Threading.Tasks;
 using WhereToFly.Core.Services;
@@ -12,13 +12,64 @@ namespace WhereToFly.Core.ViewModels
     /// </summary>
     public class SettingsViewModel : INotifyPropertyChanged
     {
+        /// <summary>
+        /// View model for map overlay type
+        /// </summary>
+        public class MapOverlayTypeViewModel
+        {
+            /// <summary>
+            /// Display text for value
+            /// </summary>
+            public string Text { get; set; }
+
+            /// <summary>
+            /// Map overlay type value
+            /// </summary>
+            public MapOverlayType Value { get; set; }
+        }
+
+        /// <summary>
+        /// View model for coordinate display format
+        /// </summary>
+        public class CoordinateDisplayFormatViewModel
+        {
+            /// <summary>
+            /// Display text for value
+            /// </summary>
+            public string Text { get; set; }
+
+            /// <summary>
+            /// Coordinate display format value
+            /// </summary>
+            public CoordinateDisplayFormat Value { get; set; }
+        }
+
+        /// <summary>
+        /// View model for map shading mode
+        /// </summary>
+        public class MapShadingModeViewModel
+        {
+            /// <summary>
+            /// Display text for value
+            /// </summary>
+            public string Text { get; set; }
+
+            /// <summary>
+            /// Map shading mode value
+            /// </summary>
+            public MapShadingMode Value { get; set; }
+        }
+
+        /// <summary>
+        /// App settings object
+        /// </summary>
         private readonly AppSettings appSettings;
 
         #region Binding properties
         /// <summary>
         /// List of available map overlay types
         /// </summary>
-        public ObservableCollection<MapOverlayType> MapOverlayTypeItems
+        public List<MapOverlayTypeViewModel> MapOverlayTypeItems
         {
             get; private set;
         }
@@ -26,24 +77,24 @@ namespace WhereToFly.Core.ViewModels
         /// <summary>
         /// Currently selected map overlay type
         /// </summary>
-        public MapOverlayType SelectedMapOverlayType
+        public MapOverlayTypeViewModel SelectedMapOverlayType
         {
             get
             {
-                return this.appSettings.MapOverlayType;
+                return this.MapOverlayTypeItems.Find(x => x.Value == this.appSettings.MapOverlayType);
             }
+
             set
             {
-                this.appSettings.MapOverlayType = value;
+                this.appSettings.MapOverlayType = value.Value;
                 Task.Factory.StartNew(async () => await this.SaveSettingsAsync());
             }
         }
 
-
         /// <summary>
         /// List of available coordinate display formats
         /// </summary>
-        public ObservableCollection<CoordinateDisplayFormat> CoordinateDisplayFormatItems
+        public List<CoordinateDisplayFormatViewModel> CoordinateDisplayFormatItems
         {
             get; private set;
         }
@@ -51,15 +102,16 @@ namespace WhereToFly.Core.ViewModels
         /// <summary>
         /// Currently selected coordinate display format
         /// </summary>
-        public CoordinateDisplayFormat SelectedCoordinateDisplayFormatItem
+        public CoordinateDisplayFormatViewModel SelectedCoordinateDisplayFormatItem
         {
             get
             {
-                return this.appSettings.CoordinateDisplayFormat;
+                return this.CoordinateDisplayFormatItems.Find(x => x.Value == this.appSettings.CoordinateDisplayFormat);
             }
+
             set
             {
-                this.appSettings.CoordinateDisplayFormat = value;
+                this.appSettings.CoordinateDisplayFormat = value.Value;
                 Task.Factory.StartNew(async () => await this.SaveSettingsAsync());
             }
         }
@@ -67,7 +119,7 @@ namespace WhereToFly.Core.ViewModels
         /// <summary>
         /// List of available map shading modes
         /// </summary>
-        public ObservableCollection<MapShadingMode> MapShadingModeItems
+        public List<MapShadingModeViewModel> MapShadingModeItems
         {
             get; private set;
         }
@@ -75,15 +127,16 @@ namespace WhereToFly.Core.ViewModels
         /// <summary>
         /// Currently selected map shading mode
         /// </summary>
-        public MapShadingMode SelectedMapShadingModeItem
+        public MapShadingModeViewModel SelectedMapShadingModeItem
         {
             get
             {
-                return this.appSettings.ShadingMode;
+                return this.MapShadingModeItems.Find(x => x.Value == this.appSettings.ShadingMode);
             }
+
             set
             {
-                this.appSettings.ShadingMode = value;
+                this.appSettings.ShadingMode = value.Value;
                 Task.Factory.StartNew(async () => await this.SaveSettingsAsync());
             }
         }
@@ -104,28 +157,31 @@ namespace WhereToFly.Core.ViewModels
         /// </summary>
         private void SetupBindings()
         {
-            this.MapOverlayTypeItems = new ObservableCollection<MapOverlayType>
+            this.MapOverlayTypeItems = new List<MapOverlayTypeViewModel>
             {
-                MapOverlayType.OpenStreetMap,
+                new MapOverlayTypeViewModel { Text = "OpenStreetMap", Value = MapOverlayType.OpenStreetMap },
             };
 
-            this.CoordinateDisplayFormatItems = new ObservableCollection<CoordinateDisplayFormat>
+            this.CoordinateDisplayFormatItems = new List<CoordinateDisplayFormatViewModel>
             {
-                CoordinateDisplayFormat.Format_dd_dddddd,
-                CoordinateDisplayFormat.Format_dd_mm_mmm,
-                CoordinateDisplayFormat.Format_dd_mm_sss,
+                new CoordinateDisplayFormatViewModel { Text = "dd.dddddd°", Value = CoordinateDisplayFormat.Format_dd_dddddd },
+                new CoordinateDisplayFormatViewModel { Text = "dd° mm.mmm'", Value = CoordinateDisplayFormat.Format_dd_mm_mmm },
+                new CoordinateDisplayFormatViewModel { Text = "dd° mm' sss\"", Value = CoordinateDisplayFormat.Format_dd_mm_sss },
             };
 
-            this.MapShadingModeItems = new ObservableCollection<MapShadingMode>
+            this.MapShadingModeItems = new List<MapShadingModeViewModel>
             {
-                MapShadingMode.Fixed10Am,
-                // TODO add others
+                new MapShadingModeViewModel { Text = "Fixed at 10 a.m.", Value = MapShadingMode.Fixed10Am },
+                ////new MapShadingModeViewModel { Text = "Fixed at 3 p.m.", Value = MapShadingMode.Fixed3Pm },
+                ////new MapShadingModeViewModel { Text = "Follow current time", Value = MapShadingMode.CurrentTime },
+                ////new MapShadingModeViewModel { Text = "Current time + 2 hours", Value = MapShadingMode.Ahead2Hours },
             };
         }
 
         /// <summary>
         /// Saves settings to data service
         /// </summary>
+        /// <returns>task to wait on</returns>
         private async Task SaveSettingsAsync()
         {
             var dataService = DependencyService.Get<DataService>();
