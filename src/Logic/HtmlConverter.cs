7ed5172df0ed0b43f5e3ff6270b0b5b2ca3227ec
Michael Fink <michael.fink@asamnet.de>
2017-12-19 20:32:08 +0100

    added documentation comments

--- src/Logic/HtmlConverter.cs
+++ src/Logic/HtmlConverter.cs
@@ -1,11 +1,15 @@
-using System.Diagnostics;
-using System.Globalization;
-using WhereToFly.Logic.Model;
-
 namespace WhereToFly.Logic
 {
+    /// <summary>
+    /// HTML conversion methods
+    /// </summary>
     public static class HtmlConverter
     {
+        /// <summary>
+        /// Converts from Markdown text to HTML
+        /// </summary>
+        /// <param name="markdownText">Markdown formatted text</param>
+        /// <returns>html text</returns>
         public static string FromMarkdown(string markdownText)
         {
             string htmlText = CommonMark.CommonMarkConverter.Convert(markdownText);
