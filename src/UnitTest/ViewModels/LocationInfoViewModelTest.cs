4d41ee99ec5f19a7e7d6c21074f29ed45a2256bc
Michael Fink <michael.fink@asamnet.de>
2018-03-18 09:59:47 +0100

    added first test for ViewModel classes

---
+++ src/UnitTest/ViewModels/LocationInfoViewModelTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhereToFly.Core.ViewModels;
+using WhereToFly.Logic.Model;
+
+namespace WhereToFly.UnitTest.ViewModels
+{
+    /// <summary>
+    /// Tests LocationInfoViewModel class
+    /// </summary>
+    [TestClass]
+    public class LocationInfoViewModelTest
+    {
+        [TestMethod]
+        public void TestCtor()
+        {
+            // set up
+            var location = new Location
+            {
+                Id = Guid.NewGuid().ToString("B"),
+                Name = "Brecherspitz",
+                Elevation = 1685,
+                MapLocation = new MapPoint(47.6764385, 11.8710533),
+                Description = "Herrliche Aussicht über die drei Seen Schliersee im Norden, Tegernsee im Westen und den Spitzingsee im Süden.",
+                Type = LocationType.Summit,
+                InternetLink = "https://de.wikipedia.org/wiki/Brecherspitz"
+            };
+
+            var viewModel = new LocationInfoViewModel(location);
+
+            // check
+            Assert.IsTrue(viewModel.Name.Length > 0, "name text must not be empty");
+            Assert.IsTrue(viewModel.Description.Length > 0, "description text must not be empty");
+        }
+    }
+}
