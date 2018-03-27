225e4d42c616bc3b135992b35e5a5bdf1b7adc70
Michael Fink <michael.fink@asamnet.de>
2018-03-27 07:14:08 +0200

    added WhereToFly.WebApi.Model assembly and live waypoint classes

---
+++ src/WebApi/Model/LiveWaypointData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace WhereToFly.WebApi.Logic
+{
+    /// <summary>
+    /// Data for a Live Waypoint
+    /// </summary>
+    public class LiveWaypointData
+    {
+        /// <summary>
+        ///  Live Waypoint ID
+        /// </summary>
+        public string ID { get; set; }
+
+        /// <summary>
+        /// Time stamp of last update to live waypoint data
+        /// </summary>
+        public DateTimeOffset TimeStamp { get; set; }
+
+        /// <summary>
+        /// Latitude value, positive values to the north
+        /// </summary>
+        public double Latitude { get; set; }
+
+        /// <summary>
+        /// Longitude value, positive values to the east
+        /// </summary>
+        public double Longitude { get; set; }
+
+        /// <summary>
+        /// Altitude value, if available; 0 means "clamp to ground"
+        /// </summary>
+        public int Altitude { get; set; }
+
+        /// <summary>
+        /// Name of live waypoint
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Detailed description, in MarkDown format
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Details link, e.g. to external website
+        /// </summary>
+        public string DetailsLink { get; set; }
+    }
+}
---
+++ src/WebApi/Model/LiveWaypointID.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace WhereToFly.WebApi.Logic
+{
+    /// <summary>
+    /// Contains a live waypoint unique ID
+    /// </summary>
+    public class LiveWaypointID
+    {
+        /// <summary>
+        /// Application prefix; set before every ID
+        /// </summary>
+        private const string AppPrefix = "wheretofly";
+
+        /// <summary>
+        /// Live Waypoint type
+        /// </summary>
+        public enum WaypointType
+        {
+            /// <summary>
+            /// Find me SPOT live location; the data part contains the SPOT feed ID
+            /// </summary>
+            FindMeSpot = 0,
+        }
+
+        /// <summary>
+        /// Type of the live waypoint
+        /// </summary>
+        public WaypointType Type { get; set; }
+
+        /// <summary>
+        /// Custom data; the meaning is specific for the different waypoint types
+        /// </summary>
+        public string Data { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Creates a new live waypoint ID object
+        /// </summary>
+        /// <param name="type">waypoint type to use</param>
+        public LiveWaypointID(WaypointType type)
+        {
+            this.Type = type;
+        }
+
+        /// <summary>
+        /// Returns a displayable text of the live waypoint ID; this can be used to transfer the
+        /// live waypoint ID over the network.
+        /// </summary>
+        /// <returns>displayable text</returns>
+        public override string ToString()
+        {
+            string type = this.Type.ToString().ToLowerInvariant();
+
+            return $"{AppPrefix}-{type}-{this.Data}";
+        }
+
+        /// <summary>
+        /// Converts a live waypoint ID in string format to a live waypoint ID object.
+        /// </summary>
+        /// <param name="id">ID to convert to object</param>
+        /// <returns>converted object</returns>
+        public static LiveWaypointID FromString(string id)
+        {
+            var parts = id.Split('-');
+            if (parts.Length != 3 ||
+                parts[0] != AppPrefix)
+            {
+                throw new FormatException("invalid Live Waypoint ID");
+            }
+
+            return new LiveWaypointID(FindWaypointTypeFromString(parts[1]))
+            {
+                Data = parts[2],
+            };
+        }
+
+        /// <summary>
+        /// Converts a string representation of a waypoint type to the enum value
+        /// </summary>
+        /// <param name="waypointType">waypoint type in all lowercase characters</param>
+        /// <returns>found waypoint type</returns>
+        private static WaypointType FindWaypointTypeFromString(string waypointType)
+        {
+            foreach (var item in Enum.GetValues(typeof(WaypointType)))
+            {
+                if (item.ToString().ToLowerInvariant() == waypointType)
+                {
+                    return (WaypointType)item;
+                }
+            }
+
+            throw new FormatException("invalid waypoint time in live waypoint ID");
+        }
+    }
+}
