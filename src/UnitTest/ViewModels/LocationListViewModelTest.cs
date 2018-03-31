90a39b322ae875f5ab3128b51f9ec0165e3cdbcd
Michael Fink <michael.fink@asamnet.de>
2018-03-31 18:07:03 +0200

    added unit test for LocationListViewModel

---
+++ src/UnitTest/ViewModels/LocationListViewModelTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using WhereToFly.Core.ViewModels;
+using WhereToFly.Logic.Model;
+
+namespace WhereToFly.UnitTest.ViewModels
+{
+    /// <summary>
+    /// Unit tests for LocationListViewModel
+    /// </summary>
+    [TestClass]
+    public class LocationListViewModelTest
+    {
+        /// <summary>
+        /// Tests ctor
+        /// </summary>
+        [TestMethod]
+        public void TestCtor()
+        {
+            // set up
+            var appSettings = new AppSettings();
+            var viewModel = new LocationListViewModel(appSettings);
+
+            // check
+            Assert.IsFalse(viewModel.LocationList.Any(), "location list is initially empty");
+            Assert.AreEqual(0, viewModel.FilterText.Length, "filter text is initially empty");
+            Assert.IsFalse(viewModel.AreAllLocationsFilteredOut, "as there are no locations, no location was filtered out");            
+        }
+    }
+}
