1651583a267133586be89eee9a96debb0c7d2ed1
Michael Fink <michael.fink@asamnet.de>
2018-04-23 21:09:11 +0200

    return "invalid" for invalid LatLongAlt

--- src/App/Geo/Spatial/LatLongAlt.cs
+++ src/App/Geo/Spatial/LatLongAlt.cs
@@ -171,7 +171,7 @@ namespace WhereToFly.App.Geo.Spatial
         /// <returns>printable text</returns>
         public override string ToString()
         {
-            return string.Format(
+            return !this.Valid ? "invalid" : string.Format(
                 "Lat={0}, Long={1}, Alt={2}",
                 this.Latitude.ToString("F6", CultureInfo.InvariantCulture),
                 this.Longitude.ToString("F6", CultureInfo.InvariantCulture),
