f10a90737d2136c1f8d01177b33549d5e644514c
Michael Fink <michael.fink@asamnet.de>
2018-06-05 21:04:18 +0200

    added waiting popup page

---
+++ src/App/Core/Views/WaitingPopupPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Rg.Plugins.Popup.Extensions;
+using Rg.Plugins.Popup.Pages;
+using System.Threading.Tasks;
+using Xamarin.Forms.Xaml;
+
+namespace WhereToFly.App.Core.Views
+{
+    /// <summary>
+    /// Popup page showing a waiting dialog. Show the dialog using ShowAsync(), hide again with
+    /// HideAsync().
+    /// </summary>
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WaitingPopupPage : PopupPage
+    {
+        /// <summary>
+        /// Creates a new waiting popup page
+        /// </summary>
+        /// <param name="waitingMessage">waiting message to display</param>
+        public WaitingPopupPage(string waitingMessage)
+        {
+            this.CloseWhenBackgroundIsClicked = false;
+
+            this.InitializeComponent();
+
+            this.waitingMessage.Text = waitingMessage;
+        }
+
+        /// <summary>
+        /// Shows popup page
+        /// </summary>
+        /// <returns>task to wait on</returns>
+        public async Task ShowAsync()
+        {
+            await this.Navigation.PushPopupAsync(this);
+        }
+
+        /// <summary>
+        /// Hides popup page
+        /// </summary>
+        /// <returns>task to wait on</returns>
+        public async Task HideAsync()
+        {
+            await this.Navigation.PopPopupAsync();
+        }
+    }
+}
