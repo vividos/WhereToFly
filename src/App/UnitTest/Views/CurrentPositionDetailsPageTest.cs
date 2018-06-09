a848b31c0fd8420cae5189940825c32ae899ce8f
Michael Fink <michael.fink@asamnet.de>
2018-06-09 16:02:03 +0200

    added some tests that are ignored for now, since CrossGeoLocator isn't available in unit tests

---
+++ src/App/UnitTest/Views/CurrentPositionDetailsPageTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhereToFly.App.Core;
+using WhereToFly.App.Core.Views;
+using WhereToFly.App.Model;
+using Xamarin.Forms;
+
+namespace WhereToFly.App.UnitTest.Views
+{
+    /// <summary>
+    /// Tests for CurrentPositionDetailsPage class
+    /// </summary>
+    [TestClass]
+    public class CurrentPositionDetailsPageTest
+    {
+        /// <summary>
+        /// Sets up tests by initializing Xamarin.Forms.Mocks
+        /// </summary>
+        [TestInitialize]
+        public void SetUp()
+        {
+            Xamarin.Forms.Mocks.MockForms.Init();
+            DependencyService.Register<IPlatform, UnitTestPlatform>();
+            Core.App.Settings = new AppSettings();
+        }
+
+        /// <summary>
+        /// Tests default ctor of CurrentPositionDetailsPage
+        /// </summary>
+        [TestMethod]
+        [Ignore("MapPage uses CrossGeoLocator.Current, which is not available in unit tests")]
+        public void TestDefaultCtor()
+        {
+            // run
+            var page = new CurrentPositionDetailsPage();
+
+            // check
+            Assert.IsTrue(page.Title.Length > 0, "page title must have been set");
+        }
+    }
+}
---
+++ src/App/UnitTest/Views/MapPageTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhereToFly.App.Core;
+using WhereToFly.App.Core.Views;
+using Xamarin.Forms;
+
+namespace WhereToFly.App.UnitTest.Views
+{
+    /// <summary>
+    /// Tests for MapPage class
+    /// </summary>
+    [TestClass]
+    public class MapPageTest
+    {
+        /// <summary>
+        /// Sets up tests by initializing Xamarin.Forms.Mocks
+        /// </summary>
+        [TestInitialize]
+        public void SetUp()
+        {
+            Xamarin.Forms.Mocks.MockForms.Init();
+            DependencyService.Register<IPlatform, UnitTestPlatform>();
+        }
+
+        /// <summary>
+        /// Tests default ctor of MapPage
+        /// </summary>
+        [TestMethod]
+        [Ignore("MapPage uses CrossGeoLocator.Current, which is not available in unit tests")]
+        public void TestDefaultCtor()
+        {
+            // set up
+            var page = new MapPage();
+
+            // check
+            Assert.IsTrue(page.Title.Length > 0, "page title must have been set");
+        }
+    }
+}
