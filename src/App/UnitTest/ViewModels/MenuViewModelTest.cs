805ee9ba53f87241570c11aa4ecd70f4526118c7
Michael Fink <michael.fink@asamnet.de>
2018-07-21 16:06:44 +0200

    added unit tests for ImportLocationsViewModel, MenuViewModel and SettingsViewModel

---
+++ src/App/UnitTest/ViewModels/MenuViewModelTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using WhereToFly.App.Core;
+using WhereToFly.App.Core.ViewModels;
+using Xamarin.Forms;
+
+namespace WhereToFly.App.UnitTest.ViewModels
+{
+    /// <summary>
+    /// Unit tests for class MenuViewModel
+    /// </summary>
+    [TestClass]
+    public class MenuViewModelTest
+    {
+        /// <summary>
+        /// Sets up tests by initializing Xamarin.Forms.Mocks and DependencyService with
+        /// DataService.
+        /// </summary>
+        [TestInitialize]
+        public void SetUp()
+        {
+            Xamarin.Forms.Mocks.MockForms.Init();
+            DependencyService.Register<IPlatform, UnitTestPlatform>();
+            DependencyService.Register<SvgImageCache>();
+
+            var imageCache = DependencyService.Get<SvgImageCache>();
+            imageCache.AddImage("icons/map.svg", string.Empty);
+            imageCache.AddImage("icons/format-list-bulleted.svg", string.Empty);
+            imageCache.AddImage("icons/compass.svg", string.Empty);
+            imageCache.AddImage("icons/weather-partlycloudy.svg", string.Empty);
+            imageCache.AddImage("icons/settings.svg", string.Empty);
+            imageCache.AddImage("icons/information-outline.svg", string.Empty);
+        }
+
+        /// <summary>
+        /// Tests default ctor of view model
+        /// </summary>
+        [TestMethod]
+        public void TestDefaultCtor()
+        {
+            // run
+            var viewModel = new MenuViewModel();
+
+            // check
+            Assert.IsTrue(viewModel.MenuItemList.Any(), "menu item list must contain items");
+
+            foreach (var menuItem in viewModel.MenuItemList)
+            {
+                var imageSource = menuItem.ImageSource;
+                Assert.IsNotNull(imageSource, "image source must not be null");
+            }
+        }
+    }
+}
---
+++ src/App/UnitTest/ViewModels/SettingsViewModelTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.Linq;
+using WhereToFly.App.Core.ViewModels;
+using WhereToFly.App.Model;
+
+namespace WhereToFly.App.UnitTest.ViewModels
+{
+    /// <summary>
+    /// Unit tests for class SettingsViewModel
+    /// </summary>
+    [TestClass]
+    public class SettingsViewModelTest
+    {
+        /// <summary>
+        /// Sets up tests by initializing app settings object
+        /// </summary>
+        [TestInitialize]
+        public void SetUp()
+        {
+            Core.App.Settings = new AppSettings();
+        }
+
+        /// <summary>
+        /// Tests default ctor of view model
+        /// </summary>
+        [TestMethod]
+        public void TestDefaultCtor()
+        {
+            // run
+            var viewModel = new SettingsViewModel();
+
+            Debug.WriteLine($"SelectedMapImageryType = {viewModel.SelectedMapImageryType}");
+            Debug.WriteLine($"SelectedMapOverlayType = {viewModel.SelectedMapOverlayType}");
+            Debug.WriteLine($"SelectedMapShadingModeItem = {viewModel.SelectedMapShadingModeItem}");
+            Debug.WriteLine($"SelectedCoordinateDisplayFormatItem = {viewModel.SelectedCoordinateDisplayFormatItem}");
+
+            viewModel.SelectedMapImageryType = viewModel.MapImageryTypeItems.Last();
+            viewModel.SelectedMapOverlayType = viewModel.MapOverlayTypeItems.Last();
+            viewModel.SelectedMapShadingModeItem = viewModel.MapShadingModeItems.Last();
+            viewModel.SelectedCoordinateDisplayFormatItem = viewModel.CoordinateDisplayFormatItems.Last();
+
+            // check
+            Assert.IsTrue(viewModel.MapImageryTypeItems.Any(), "map imagery type list must contain items");
+            Assert.IsTrue(viewModel.MapOverlayTypeItems.Any(), "map overlay type list must contain items");
+            Assert.IsTrue(viewModel.MapShadingModeItems.Any(), "map shading mode list must contain items");
+            Assert.IsTrue(viewModel.CoordinateDisplayFormatItems.Any(), "coordinate display format list must contain items");
+        }
+    }
+}
