7381618207a2eb9fccf98659aa102dd87a1fcc9b
Michael Fink <michael.fink@asamnet.de>
2018-08-07 21:07:47 +0200

    using LiveWaypointCacheManager in LiveWaypointController

--- src/WebApi/Core/Controllers/LiveWaypointController.cs
+++ src/WebApi/Core/Controllers/LiveWaypointController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using WhereToFly.WebApi.Logic.Services;
+using Microsoft.Extensions.Logging;
+using WhereToFly.WebApi.Logic;
 
 namespace LiveWaypoints.Controllers
 {
@@ -13,14 +14,35 @@ namespace LiveWaypoints.Controllers
     public class LiveWaypointController : Controller
     {
         /// <summary>
-        /// GET api/values/5
+        /// Logging instance
+        /// </summary>
+        private readonly ILogger<LiveWaypointController> logger;
+
+        /// <summary>
+        /// Live waypoint cache manager
+        /// </summary>
+        private readonly LiveWaypointCacheManager cacheManager;
+
+        /// <summary>
+        /// Creates a new controller to get live waypoint data
+        /// </summary>
+        /// <param name="logger">logger instance to use</param>
+        /// <param name="cacheManager">cache manager to use</param>
+        public LiveWaypointController(ILogger<LiveWaypointController> logger, LiveWaypointCacheManager cacheManager)
+        {
+            this.logger = logger;
+            this.cacheManager = cacheManager;
+        }
+
+        /// <summary>
+        /// GET api/livewaypoint/id
         /// Retrieves the current live waypoint infos for the given ID. The ID completely
         /// describes the live waypoint. The latest data for the waypoint is retrieved when
         /// necessary, or a cached copy of the data.
         /// </summary>
         /// <param name="id">the Live Waypoint ID</param>
         /// <returns>live waypoint data</returns>
-        /// <response code="400">An invalid live waypoint ID was passed</response>  
+        /// <response code="400">An invalid live waypoint ID was passed</response>
         [HttpGet("{id}")]
         [ProducesResponseType(typeof(string), 200)]
         [ProducesResponseType(typeof(string), 400)]
@@ -28,14 +50,15 @@ namespace LiveWaypoints.Controllers
         {
             if (string.IsNullOrEmpty(id))
             {
+                logger.LogWarning($"invalid live waypoint ID: {id}");
                 return this.BadRequest();
             }
 
-            var service = new FindMeSpotTrackerDataService();
+            logger.LogDebug($"getting live waypoint with ID: {id}");
 
-            var liveWaypointData = service.GetDataAsync(id);
+            var liveWaypointData = cacheManager.GetLiveWaypointData(id);
 
-            return new JsonResult(liveWaypointData);
+            return new JsonResult(liveWaypointData.Result);
         }
     }
 }
