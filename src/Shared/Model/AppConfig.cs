0eea9aa43c9b728c097e627d1375f599757b10d8
Michael Fink <michael.fink@asamnet.de>
2018-08-08 21:23:14 +0200

    added AppConfig class and WebApi controller to return app config

---
+++ src/Shared/Model/AppConfig.cs
@@ -0,0 +1,18 @@
+﻿namespace WhereToFly.Shared.Model
+{
+    /// <summary>
+    /// Configuration data for the WhereToFly app
+    /// </summary>
+    public class AppConfig
+    {
+        /// <summary>
+        /// API key for Cesium ION
+        /// </summary>
+        public string CesiumIonApiKey { get; set; }
+
+        /// <summary>
+        /// API key for Bing maps
+        /// </summary>
+        public string BingMapsApiKey { get; set;}
+    }
+}
---
+++ src/WebApi/Core/Controllers/AppConfigController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WhereToFly.Shared.Model;
+
+namespace WhereToFly.WebApi.Core.Controllers
+{
+    /// <summary>
+    /// Controller to deliver app config for WhereToiFly app
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppConfigController : Controller
+    {
+        /// <summary>
+        /// Logger instance
+        /// </summary>
+        private readonly ILogger<AppConfigController> logger;
+
+        /// <summary>
+        /// App config object to use
+        /// </summary>
+        private readonly AppConfig appConfig;
+
+        /// <summary>
+        /// Creates a new app config controller
+        /// </summary>
+        /// <param name="logger">logging instance</param>
+        public AppConfigController(ILogger<AppConfigController> logger)
+        {
+            this.logger = logger;
+
+            this.appConfig = new AppConfig
+            {
+                CesiumIonApiKey = string.Empty,
+                BingMapsApiKey = string.Empty,
+            };
+        }
+
+        /// <summary>
+        /// GET: api/AppConfig/appVersion
+        /// Returns current WhereToFly app configuration.
+        /// </summary>
+        /// <param name="appVersion">version of the app that wants to get app config</param>
+        /// <returns>app config object</returns>
+        [HttpGet]
+        public AppConfig Get(string appVersion)
+        {
+            this.logger.LogInformation($"WhereToFly app config request, with version {appVersion}");
+
+            return this.appConfig;
+        }
+    }
+}
