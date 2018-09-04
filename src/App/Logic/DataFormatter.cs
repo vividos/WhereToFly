7581f3411f87d0a6fbacb36baeb0dc6ceb33497a
Michael Fink <michael.fink@asamnet.de>
2018-09-04 11:47:27 +0200

    fixed formatting negative latitude and longitude values

--- src/App/Logic/DataFormatter.cs
+++ src/App/Logic/DataFormatter.cs
@@ -24,11 +24,11 @@ namespace WhereToFly.App.Logic
                     return latLong.ToString("F6", CultureInfo.InvariantCulture);
 
                 case CoordinateDisplayFormat.Format_dd_mm_mmm:
-                    double fractionalPart = (latLong - (int)latLong) * 60.0;
+                    double fractionalPart = Math.Abs((latLong - (int)latLong) * 60.0);
                     return string.Format(CultureInfo.InvariantCulture, "{0}° {1:F3}'", (int)latLong, fractionalPart);
 
                 case CoordinateDisplayFormat.Format_dd_mm_sss:
-                    double minutePart = (latLong - (int)latLong) * 60.0;
+                    double minutePart = Math.Abs((latLong - (int)latLong) * 60.0);
                     double secondsPart = (minutePart - (int)minutePart) * 60.0;
                     return string.Format(CultureInfo.InvariantCulture, "{0}° {1}' {2}\"", (int)latLong, (int)minutePart, (int)secondsPart);
 
--- src/App/UnitTest/DataFormatterTest.cs
+++ src/App/UnitTest/DataFormatterTest.cs
@@ -32,6 +32,26 @@ namespace WhereToFly.App.UnitTest
         }
 
         /// <summary>
+        /// Tests function FormatLatLong() with negative value
+        /// </summary>
+        [TestMethod]
+        public void TestFormatNegativeLatLong()
+        {
+            // set up
+            double latLong = -47.6764385;
+
+            // run
+            string text1 = DataFormatter.FormatLatLong(latLong, CoordinateDisplayFormat.Format_dd_dddddd);
+            string text2 = DataFormatter.FormatLatLong(latLong, CoordinateDisplayFormat.Format_dd_mm_mmm);
+            string text3 = DataFormatter.FormatLatLong(latLong, CoordinateDisplayFormat.Format_dd_mm_sss);
+
+            // check
+            Assert.AreEqual("-47.676439", text1, "formatted text must match");
+            Assert.AreEqual("-47° 40.586'", text2, "formatted text must match");
+            Assert.AreEqual("-47° 40' 35\"", text3, "formatted text must match");
+        }
+
+        /// <summary>
         /// Tests function FormatMyPositionShareText()
         /// </summary>
         [TestMethod]
