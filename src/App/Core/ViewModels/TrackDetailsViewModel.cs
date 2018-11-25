127b626517a081e80ca5dd5a1135d99bf9aa36a4
Michael Fink <michael.fink@asamnet.de>
2018-11-25 20:13:29 +0100

    showing new Track properties in track details page

--- src/App/Core/ViewModels/TrackDetailsViewModel.cs
+++ src/App/Core/ViewModels/TrackDetailsViewModel.cs
@@ -70,13 +70,101 @@ namespace WhereToFly.App.Core.ViewModels
         }
 
         /// <summary>
+        /// Property containing number of track points
+        /// </summary>
+        public int NumTrackPoints
+        {
+            get
+            {
+                return this.track.TrackPoints.Count;
+            }
+        }
+
+        /// <summary>
+        /// Property containing height gain
+        /// </summary>
+        public string HeightGain
+        {
+            get
+            {
+                return this.track.HeightGain.ToString("F1") + " m";
+            }
+        }
+
+        /// <summary>
+        /// Property containing height loss
+        /// </summary>
+        public string HeightLoss
+        {
+            get
+            {
+                return this.track.HeightLoss.ToString("F1") + " m";
+            }
+        }
+
+        /// <summary>
+        /// Property containing maximum height
+        /// </summary>
+        public string MaxHeight
+        {
+            get
+            {
+                return this.track.MaxHeight.ToString("F1") + " m";
+            }
+        }
+
+        /// <summary>
+        /// Property containing minimum height
+        /// </summary>
+        public string MinHeight
+        {
+            get
+            {
+                return this.track.MinHeight.ToString("F1") + " m";
+            }
+        }
+
+        /// <summary>
         /// Property containing max. climb rate
         /// </summary>
         public string MaxClimbRate
         {
             get
             {
-                return this.track.MaxClimbRate.ToString("F1");
+                return this.track.MaxClimbRate.ToString("F1") + " m/s";
+            }
+        }
+
+        /// <summary>
+        /// Property containing max. sink rate
+        /// </summary>
+        public string MaxSinkRate
+        {
+            get
+            {
+                return this.track.MaxSinkRate.ToString("F1") + " m/s";
+            }
+        }
+
+        /// <summary>
+        /// Property containing max. speed
+        /// </summary>
+        public string MaxSpeed
+        {
+            get
+            {
+                return this.track.MaxSpeed.ToString("F1") + " km/h";
+            }
+        }
+
+        /// <summary>
+        /// Property containing average speed
+        /// </summary>
+        public string AverageSpeed
+        {
+            get
+            {
+                return this.track.AverageSpeed.ToString("F1") + " km/h";
             }
         }
 
