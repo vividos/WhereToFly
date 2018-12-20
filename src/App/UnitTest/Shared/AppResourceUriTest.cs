7b8d8431959ac68177c0d28d9dcb04129a64b32e
Michael Fink <michael.fink@asamnet.de>
2018-12-20 06:57:54 +0100

    added shared class AppResourceUri

---
+++ src/App/UnitTest/Shared/AppResourceUriTest.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using WhereToFly.Shared.Model;
+
+namespace WhereToFly.App.UnitTest.Shared
+{
+    /// <summary>
+    /// Tests for class AppResourceUri
+    /// </summary>
+    [TestClass]
+    public class AppResourceUriTest
+    {
+        /// <summary>
+        /// Tests constructing an AppResourceUri from an URI string
+        /// </summary>
+        [TestMethod]
+        public void TestCreateUriFromString()
+        {
+            // set up
+            string uriText1 = "where-to-fly://FindMeSpotPos/xxx";
+            string uriText2 = "where-to-fly://GarminInreachPos/yyy";
+
+            // run
+            var uri1 = new AppResourceUri(uriText1);
+            var uri2 = new AppResourceUri(uriText2);
+
+            // check
+            Assert.IsTrue(uri1.IsValid, "uri1 must be valid");
+            Assert.IsTrue(uri2.IsValid, "uri2 must be valid");
+
+            Assert.IsTrue(uriText1.ToLowerInvariant() == uri1.ToString(), "uri text and ToString() result must match");
+            Assert.IsTrue(uriText2.ToLowerInvariant() == uri2.ToString(), "uri text and ToString() result must match");
+        }
+
+        /// <summary>
+        /// Tests constructing an AppResourceUri from resource type and data
+        /// </summary>
+        [TestMethod]
+        public void TestCreateUriFromTypeAndData()
+        {
+            // run
+            var uri1 = new AppResourceUri(AppResourceUri.ResourceType.FindMeSpotPos, "xxx");
+            var uri2 = new AppResourceUri(AppResourceUri.ResourceType.GarminInreachPos, "yyy");
+
+            // check
+            Assert.IsTrue(uri1.IsValid, "uri1 must be valid");
+            Assert.IsTrue(uri2.IsValid, "uri2 must be valid");
+
+            Assert.AreEqual("where-to-fly://findmespotpos/xxx", uri1.ToString(), "uri text and ToString() result must match");
+            Assert.AreEqual("where-to-fly://garmininreachpos/yyy", uri2.ToString(), "uri text and ToString() result must match");
+        }
+
+        /// <summary>
+        /// Tests parsing invalid Uris
+        /// </summary>
+        [TestMethod]
+        public void TestParseInvalidUris()
+        {
+            // run
+            Assert.ThrowsException<ArgumentNullException>(() => new AppResourceUri(null));
+            Assert.ThrowsException<ArgumentException>(() => new AppResourceUri(AppResourceUri.ResourceType.None, "123"));
+            Assert.ThrowsException<ArgumentNullException>(() => new AppResourceUri(AppResourceUri.ResourceType.FindMeSpotPos, null));
+            Assert.ThrowsException<UriFormatException>(() => new AppResourceUri(string.Empty));
+
+            var uri1 = new AppResourceUri("http://github.com/");
+            var uri2 = new AppResourceUri("where-to-fly://");
+            var uri3 = new AppResourceUri("where-to-fly://xxx");
+            var uri4 = new AppResourceUri("where-to-fly://findmespotpos/");
+
+            // check
+            Assert.IsFalse(uri1.IsValid, "uri must be invalid");
+            Assert.IsFalse(uri2.IsValid, "uri must be invalid");
+            Assert.IsFalse(uri3.IsValid, "uri must be invalid");
+            Assert.IsFalse(uri4.IsValid, "uri must be invalid");
+
+            Assert.AreEqual("invalid", uri1.ToString(), "invalid URI must return proper ToString() result");
+        }
+
+        /// <summary>
+        /// Tests equality of AppResourceUri objects
+        /// </summary>
+        [TestMethod]
+        public void TestEquality()
+        {
+            // set up
+            var uri1 = new AppResourceUri(AppResourceUri.ResourceType.FindMeSpotPos, "xxx");
+            var uri2 = new AppResourceUri(AppResourceUri.ResourceType.GarminInreachPos, "yyy");
+            var uri3 = new AppResourceUri(AppResourceUri.ResourceType.GarminInreachPos, "yyy");
+
+            // run
+            Assert.AreEqual<object>(uri1, uri1, "same objects must be equal");
+            Assert.AreNotEqual<object>(uri1, uri2, "different objects must be equal");
+            Assert.AreEqual<object>(uri2, uri3, "different references must be equal");
+            Assert.AreNotEqual<object>("hello", uri1, "different object types must not be equal");
+
+            Assert.AreEqual<AppResourceUri>(uri1, uri1, "same objects must be equal");
+            Assert.AreNotEqual<AppResourceUri>(uri1, uri2, "different objects must be equal");
+            Assert.AreEqual<AppResourceUri>(uri2, uri3, "different references must be equal");
+            Assert.AreNotEqual<AppResourceUri>(uri1, null, "object must not be equal to null");
+
+            Assert.AreEqual(uri2.GetHashCode(), uri3.GetHashCode(), "hash codes of same objects must be equal");
+        }
+
+        /// <summary>
+        /// Tests serializing and deserializing AppResourceUri objects
+        /// </summary>
+        [TestMethod]
+        public void TestSerializeDeserialize()
+        {
+            // set up
+            var uri = new AppResourceUri("where-to-fly://GarminInreachPos/yyy");
+
+            // run
+            string json = JsonConvert.SerializeObject(uri);
+            AppResourceUri uri2 = JsonConvert.DeserializeObject<AppResourceUri>(json);
+
+            // check
+            Assert.IsTrue(uri2.IsValid, "deserialized uri must be valid");
+            Assert.AreEqual(uri, uri2, "uris must be equal");
+        }
+
+        /// <summary>
+        /// Tests app resource URI as key in dictionies
+        /// </summary>
+        public void TestKeyInDictionary()
+        {
+            // set up
+            var uri1 = new AppResourceUri(AppResourceUri.ResourceType.FindMeSpotPos, "xxx");
+            var uri2 = new AppResourceUri(AppResourceUri.ResourceType.GarminInreachPos, "yyy");
+            var uri3 = new AppResourceUri(AppResourceUri.ResourceType.GarminInreachPos, "yyy");
+
+            var dict = new Dictionary<AppResourceUri, int>();
+
+            // run + check
+            Assert.IsFalse(dict.Any(), "there must be no items in the dictionary");
+
+            dict[uri1] = 42;
+            Assert.IsTrue(dict.Any(), "there must be an item in the dictionary");
+            Assert.IsTrue(dict.ContainsKey(uri1), "dict must contain value for uri1");
+            Assert.IsFalse(dict.ContainsKey(uri2), "dict must not contain value for uri1");
+
+            dict[uri2] = 64;
+            Assert.AreEqual(2, dict.Count, "there must be 2 items in the dictionary");
+
+            dict[uri3] = 128;
+            Assert.AreEqual(2, dict.Count, "there must still be 2 items in the dictionary");
+
+            Assert.AreEqual(42, dict[uri1], "value of key with uri 1 must match");
+            Assert.AreEqual(128, dict[uri2], "value of key with uri 2 must match");
+            Assert.AreEqual(128, dict[uri3], "value of key with uri 3 must match");
+        }
+    }
+}
---
+++ src/Shared/Model/AppResourceUri.cs
@@ -0,0 +1,216 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace WhereToFly.Shared.Model
+{
+    /// <summary>
+    /// Represents an Uniform Resource Locator (URI) for the Where-to-fly app.
+    /// </summary>
+    [JsonConverter(typeof(AppResourceUri.Converter))]
+    public sealed class AppResourceUri : IEquatable<AppResourceUri>
+    {
+        /// <summary>
+        /// Default URI scheme for App URIs
+        /// </summary>
+        public static readonly string DefaultScheme = "where-to-fly";
+
+        /// <summary>
+        /// Stores the actual URI
+        /// </summary>
+        private readonly Uri uri;
+
+        /// <summary>
+        /// Resource type
+        /// </summary>
+        public enum ResourceType
+        {
+            /// <summary>
+            /// No resource; invalid app resource URI
+            /// </summary>
+            None = -1,
+
+            /// <summary>
+            /// Uri specifies a "Find me SPOT" position; the data property contains the SPOT feed
+            /// ID.
+            /// </summary>
+            FindMeSpotPos = 0,
+
+            /// <summary>
+            /// Specifies a "Garmin inReach" position; the data property contains the MapShare
+            /// identifier.
+            /// </summary>
+            GarminInreachPos = 1,
+        }
+
+        /// <summary>
+        /// Property that returns if the URI is valid
+        /// </summary>
+        public bool IsValid
+        {
+            get
+            {
+                return this.uri != null &&
+                    this.uri.Scheme == DefaultScheme &&
+                    this.Type != ResourceType.None &&
+                    this.Data != null;
+            }
+        }
+
+        /// <summary>
+        /// Resource type of the app resource URI; invalid when it contains None.
+        /// </summary>
+        public ResourceType Type { get; private set; } = ResourceType.None;
+
+        /// <summary>
+        /// Custom data; the meaning is specific for different resource types. This property is
+        /// only valid when not null.
+        /// </summary>
+        public string Data { get; private set; } = null;
+
+        /// <summary>
+        /// Creates a new app resource URI object from a given URI to parse
+        /// </summary>
+        /// <param name="uriToParse">URI to parse</param>
+        public AppResourceUri(string uriToParse)
+        {
+            if (uriToParse == null)
+            {
+                throw new ArgumentNullException(nameof(uriToParse));
+            }
+
+            this.uri = new Uri(uriToParse);
+            this.ParseUri(this.uri);
+        }
+
+        /// <summary>
+        /// Creates a new app resource URI object from resource type and data
+        /// </summary>
+        /// <param name="type">resource type to use</param>
+        /// <param name="data">data to use</param>
+        public AppResourceUri(ResourceType type, string data)
+        {
+            if (type == ResourceType.None)
+            {
+                throw new ArgumentException("invalid resource type", nameof(type));
+            }
+
+            this.Type = type;
+            this.Data = data ?? throw new ArgumentNullException(nameof(data));
+
+            this.uri = new Uri(this.FormatUri());
+        }
+
+        /// <summary>
+        /// Parses given Uri object parts and sets Type and Data properties.
+        /// </summary>
+        /// <param name="uri">Uri object to parse</param>
+        private void ParseUri(Uri uri)
+        {
+            if (!string.IsNullOrEmpty(uri.Host))
+            {
+                this.Type = FindResourceTypeFromString(uri.Host);
+            }
+
+            if (uri.Segments.Length == 2 &&
+                uri.Segments[0] == "/")
+            {
+                this.Data = uri.Segments[1];
+            }
+        }
+
+        /// <summary>
+        /// Converts a string representation of a resource type to an enum value
+        /// </summary>
+        /// <param name="resourceType">resource type in all lowercase characters</param>
+        /// <returns>found resource type, or None when it is unknown</returns>
+        private static ResourceType FindResourceTypeFromString(string resourceType)
+        {
+            foreach (var item in Enum.GetValues(typeof(ResourceType)))
+            {
+                if (item.ToString().ToLowerInvariant() == resourceType)
+                {
+                    return (ResourceType)item;
+                }
+            }
+
+            return ResourceType.None;
+        }
+
+        /// <summary>
+        /// Returns a displayable text of the app resouce URI; this can also be used to transfer
+        /// the app resource URI over the network.
+        /// </summary>
+        /// <returns>displayable text</returns>
+        public override string ToString() => this.IsValid ? this.FormatUri() : "invalid";
+
+        /// <summary>
+        /// Formats URI from object properties, without validity checking
+        /// </summary>
+        /// <returns>formatted app resource URI</returns>
+        private string FormatUri()
+        {
+            string type = this.Type.ToString().ToLowerInvariant();
+
+            return $"{DefaultScheme}://{type}/{this.Data}";
+        }
+
+        /// <summary>
+        /// Returns hash code for app resource URI
+        /// </summary>
+        /// <returns>calculated hash code</returns>
+        public override int GetHashCode() => this.uri.GetHashCode();
+
+        /// <summary>
+        /// Compares this app resource URI to another object
+        /// </summary>
+        /// <param name="obj">object to compare to</param>
+        /// <returns>true when equal URIs, false when not</returns>
+        public override bool Equals(object obj) => obj is AppResourceUri other && this.Equals(other);
+
+        /// <summary>
+        /// Compares this app resource URI to another app resource URI
+        /// </summary>
+        /// <param name="other">other URI to compare to</param>
+        /// <returns>true when equal URIs, false when not</returns>
+        public bool Equals(AppResourceUri other) => this.uri == other.uri;
+
+        /// <summary>
+        /// Nested JSON converter class for app resource URI
+        /// </summary>
+        private class Converter : JsonConverter
+        {
+            /// <summary>
+            /// Determines if given type can be converted to an app resource URI
+            /// </summary>
+            /// <param name="objectType">object type to convert to</param>
+            /// <returns>true when type can be converted to, false when not</returns>
+            public override bool CanConvert(Type objectType) => typeof(AppResourceUri).IsAssignableFrom(objectType);
+
+            /// <summary>
+            /// Reads app resource URI from JSON
+            /// </summary>
+            /// <param name="reader">json reader</param>
+            /// <param name="objectType">type of object to read/return</param>
+            /// <param name="existingValue">existing value; unused</param>
+            /// <param name="serializer">json serializer</param>
+            /// <returns>created app resource URI object, or null when reading failed</returns>
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+            {
+                string uriAsText = serializer.Deserialize<string>(reader);
+                return string.IsNullOrEmpty(uriAsText) ? null : new AppResourceUri(uriAsText);
+            }
+
+            /// <summary>
+            /// Writes app resource URI to JSON
+            /// </summary>
+            /// <param name="writer">json writer</param>
+            /// <param name="value">app resource URI object to write</param>
+            /// <param name="serializer">json serializer</param>
+            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+            {
+                var uri = value as AppResourceUri;
+                writer.WriteValue(uri?.ToString() ?? string.Empty);
+            }
+        }
+    }
+}
