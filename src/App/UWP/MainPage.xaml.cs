49b146b90086ee177659302d1fbef378cf6b124f
Michael Fink <michael.fink@asamnet.de>
2019-03-28 22:45:49 +0100

    set title bar color for UWP app

--- src/App/UWP/MainPage.xaml.cs
+++ src/App/UWP/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace WhereToFly.App.UWP
+﻿using Windows.UI.ViewManagement;
+
+namespace WhereToFly.App.UWP
 {
     /// <summary>
     /// Main page of the UWP app
@@ -10,6 +12,10 @@
         /// </summary>
         public MainPage()
         {
+            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.BackgroundColor = titleBar.ButtonBackgroundColor =
+                Windows.UI.Color.FromArgb(0xFF, 0x2F, 0x29, 0x9E);
+
             this.InitializeComponent();
 
             this.LoadApplication(new Core.App());
