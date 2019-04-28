5f6fbf858f3847fda9979103d4784fe9057c6576
Michael Fink <michael.fink@asamnet.de>
2019-04-28 21:56:10 +0200

    added FlyingRangeParameters class

---
+++ src/App/Model/FlyingRangeParameters.cs
@@ -0,0 +1,28 @@
+﻿namespace WhereToFly.App.Model
+{
+    /// <summary>
+    /// Parameters for the flying range calculation
+    /// </summary>
+    public class FlyingRangeParameters
+    {
+        /// <summary>
+        /// Specifies the glide ratio, in km gliding per 1000m sinking.
+        /// </summary>
+        public double GlideRatio { get; set; } = 6.0;
+
+        /// <summary>
+        /// Glider speed, in km/h; needed for calculating glide factor against wind
+        /// </summary>
+        public object GliderSpeed { get; set; } = 38.0;
+
+        /// <summary>
+        /// Wind speed, in km/h
+        /// </summary>
+        public double WindSpeed { get; set; } = 0.0;
+
+        /// <summary>
+        /// Wind direction, in degrees; 0° means North
+        /// </summary>
+        public double WindDirection { get; set; } = 0.0;
+    }
+}
