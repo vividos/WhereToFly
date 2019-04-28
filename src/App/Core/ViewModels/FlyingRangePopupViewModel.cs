adb2196ce8d7e7fe02cfd3fece982d5f99cc89ea
Michael Fink <michael.fink@asamnet.de>
2019-04-28 22:00:05 +0200

    added new popup page for configuring flying range parameters; picker for wind direction and wind speed are currently disabled

---
+++ src/App/Core/ViewModels/FlyingRangePopupViewModel.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using WhereToFly.App.Model;
+
+namespace WhereToFly.App.Core.ViewModels
+{
+    /// <summary>
+    /// View model for the "flying range" popup page
+    /// </summary>
+    public class FlyingRangePopupViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Flying range parameters
+        /// </summary>
+        public FlyingRangeParameters Parameters { get; private set; } = new FlyingRangeParameters();
+
+        #region Binding properties
+        /// <summary>
+        /// Property that contains the glide ratio value
+        /// </summary>
+        public double GlideRatio
+        {
+            get => this.Parameters.GlideRatio;
+            set
+            {
+                this.Parameters.GlideRatio = value;
+                this.OnPropertyChanged(nameof(GlideRatio));
+            }
+        }
+
+        /// <summary>
+        /// List of possible wind directions
+        /// </summary>
+        public List<string> WindDirectionList { get; private set; }
+
+        /// <summary>
+        /// Property that contains the wind direction as printable wind direction
+        /// </summary>
+        public string WindDirection
+        {
+            get
+            {
+                int index = (int)((this.Parameters.WindDirection % 360.0) / 45.0);
+                Debug.Assert(index < this.WindDirectionList.Count, "index must be in array length");
+                return this.WindDirectionList[index];
+            }
+
+            set
+            {
+                int index = this.WindDirectionList.IndexOf(value);
+                this.Parameters.WindDirection = index * 45.0;
+            }
+        }
+
+        /// <summary>
+        /// Property that contains the wind speed
+        /// </summary>
+        public string WindSpeed
+        {
+            get => $"{(int)this.Parameters.WindSpeed} km/h";
+            set
+            {
+                int windSpeed = 0;
+                if (int.TryParse(value.Replace(" km/h", string.Empty), out windSpeed))
+                {
+                    this.Parameters.WindSpeed = windSpeed;
+                }
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// Creates a new "flying range" popup page view model
+        /// </summary>
+        public FlyingRangePopupViewModel()
+        {
+            // the wind direction list is specified so that the direction angle can directly be
+            // calculated from the entries index
+            this.WindDirectionList = new List<string>
+            {
+                "N",
+                "NE",
+                "E",
+                "SE",
+                "S",
+                "SW",
+                "W",
+                "NW",
+            };
+        }
+    }
+}
---
+++ src/App/Core/Views/FlyingRangePopupPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using Rg.Plugins.Popup.Extensions;
+using Rg.Plugins.Popup.Pages;
+using System;
+using System.Threading.Tasks;
+using WhereToFly.App.Core.ViewModels;
+using WhereToFly.App.Model;
+using Xamarin.Forms.Xaml;
+
+namespace WhereToFly.App.Core.Views
+{
+    /// <summary>
+    /// Popup page for setting parameters to show flying range.
+    /// </summary>
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FlyingRangePopupPage : PopupPage
+    {
+        /// <summary>
+        /// View model for this popup page
+        /// </summary>
+        private readonly FlyingRangePopupViewModel viewModel;
+
+        /// <summary>
+        /// Task completion source to report back flying range parameters
+        /// </summary>
+        private TaskCompletionSource<FlyingRangeParameters> tcs;
+
+        /// <summary>
+        /// Creates a new popup page to edit flying range parameters
+        /// </summary>
+        public FlyingRangePopupPage()
+        {
+            this.CloseWhenBackgroundIsClicked = true;
+
+            this.InitializeComponent();
+
+            this.BindingContext = this.viewModel = new FlyingRangePopupViewModel();
+        }
+
+        /// <summary>
+        /// Shows "flying parameters" popup page and lets the user edit the values.
+        /// </summary>
+        /// <returns>flying range parameters</returns>
+        public static async Task<FlyingRangeParameters> ShowAsync()
+        {
+            var popupPage = new FlyingRangePopupPage()
+            {
+                tcs = new TaskCompletionSource<FlyingRangeParameters>()
+            };
+
+            await popupPage.Navigation.PushPopupAsync(popupPage);
+
+            return await popupPage.tcs.Task;
+        }
+
+        /// <summary>
+        /// Called when user clicked on the background, dismissing the popup page.
+        /// </summary>
+        /// <returns>whatever the base class returns</returns>
+        protected override bool OnBackgroundClicked()
+        {
+            if (!this.tcs.Task.IsCompleted)
+            {
+                this.tcs.SetResult(null);
+            }
+
+            return base.OnBackgroundClicked();
+        }
+
+        /// <summary>
+        /// Called when user naviaged back with the back button, dismissing the popup page.
+        /// </summary>
+        /// <returns>whatever the base class returns</returns>
+        protected override bool OnBackButtonPressed()
+        {
+            if (!this.tcs.Task.IsCompleted)
+            {
+                this.tcs.SetResult(null);
+            }
+
+            return base.OnBackButtonPressed();
+        }
+
+        /// <summary>
+        /// Called when user clicked on the "Show flying range" button, ending the popup page.
+        /// </summary>
+        /// <param name="sender">sender object</param>
+        /// <param name="args">event args</param>
+        private async void OnClickedShowFlyingRange(object sender, EventArgs args)
+        {
+            if (!this.tcs.Task.IsCompleted)
+            {
+                this.tcs.SetResult(this.viewModel.Parameters);
+            }
+
+            await this.Navigation.PopPopupAsync();
+        }
+    }
+}
