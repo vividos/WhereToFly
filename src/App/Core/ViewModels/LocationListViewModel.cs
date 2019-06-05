9308730ac449080ceebe0a3855e9ead78a259217
Michael Fink <michael.fink@asamnet.de>
2019-06-05 20:36:07 +0200

    corrected link to vividos' Where-to-fly resources

--- src/App/Core/ViewModels/LocationListViewModel.cs
+++ src/App/Core/ViewModels/LocationListViewModel.cs
@@ -35,7 +35,7 @@ namespace WhereToFly.App.Core.ViewModels
         /// </summary>
         private readonly Dictionary<string, string> downloadWebSiteList = new Dictionary<string, string>
         {
-            { "vividos' Where-to-fly resources", "http://www.vividos.de/wheretofly.html" },
+            { "vividos' Where-to-fly resources", "http://www.vividos.de/wheretofly/index.html" },
             { "Paraglidingspots.com", "http://paraglidingspots.com/downloadselect.aspx" },
             { "DHV Gelände-Datenbank",
                 "https://www.dhv.de/web/piloteninfos/gelaende-luftraum-natur/fluggelaendeflugbetrieb/gelaendedaten/gelaendedaten-download" }
