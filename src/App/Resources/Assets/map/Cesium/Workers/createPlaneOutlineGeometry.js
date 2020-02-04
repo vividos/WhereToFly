eb470a6e21bceff51b293eb76c37afe16caf2357
Michael Fink <michael.fink@asamnet.de>
2020-02-04 07:51:22 +0100

    updated CesiumJS to version 1.66

--- src/App/Resources/Assets/map/Cesium/Workers/createPlaneOutlineGeometry.js
+++ src/App/Resources/Assets/map/Cesium/Workers/createPlaneOutlineGeometry.js
@@ -1,7 +1,7 @@
 /**
  * Cesium - https://github.com/AnalyticalGraphicsInc/cesium
  *
- * Copyright 2011-2017 Cesium Contributors
+ * Copyright 2011-2020 Cesium Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,4 +20,4 @@
  * Portions licensed separately.
  * See https://github.com/AnalyticalGraphicsInc/cesium/blob/master/LICENSE.md for full licensing details.
  */
-define(["./defined-2a4f2d00","./Check-e5651467","./freezeObject-a51e076f","./defaultValue-29c9b1af","./Math-7782f09e","./Cartesian2-ba70b51f","./defineProperties-c817531e","./Transforms-5119c07b","./RuntimeError-51c34ab4","./WebGLConstants-90dbfe2f","./ComponentDatatype-418b1c61","./GeometryAttribute-8bc1900e","./when-1faa3867","./GeometryAttributes-f8548d3f"],function(r,e,t,n,a,i,o,u,c,f,s,y,p,b){"use strict";function m(){this._workerName="createPlaneOutlineGeometry"}m.packedLength=0,m.pack=function(e,t){return t},m.unpack=function(e,t,n){return r.defined(n)?n:new m};var d=new i.Cartesian3(-.5,-.5,0),w=new i.Cartesian3(.5,.5,0);return m.createGeometry=function(){var e=new b.GeometryAttributes,t=new Uint16Array(8),n=new Float64Array(12);return n[0]=d.x,n[1]=d.y,n[2]=d.z,n[3]=w.x,n[4]=d.y,n[5]=d.z,n[6]=w.x,n[7]=w.y,n[8]=d.z,n[9]=d.x,n[10]=w.y,n[11]=d.z,e.position=new y.GeometryAttribute({componentDatatype:s.ComponentDatatype.DOUBLE,componentsPerAttribute:3,values:n}),t[0]=0,t[1]=1,t[2]=1,t[3]=2,t[4]=2,t[5]=3,t[6]=3,t[7]=0,new y.Geometry({attributes:e,indices:t,primitiveType:y.PrimitiveType.LINES,boundingSphere:new u.BoundingSphere(i.Cartesian3.ZERO,Math.sqrt(2))})},function(e,t){return r.defined(t)&&(e=m.unpack(e,t)),m.createGeometry(e)}});
+define(["./when-0488ac89","./Check-78ca6843","./Math-8a4c9da1","./Cartesian2-cc1e6450","./defineProperties-c6a70625","./Transforms-fa4f10bc","./RuntimeError-4d6e0952","./WebGLConstants-66e14a3b","./ComponentDatatype-9252f28f","./GeometryAttribute-3345e440","./GeometryAttributes-3227db5b"],function(r,e,t,a,n,i,o,c,u,s,y){"use strict";function p(){this._workerName="createPlaneOutlineGeometry"}p.packedLength=0,p.pack=function(e,t){return t},p.unpack=function(e,t,n){return r.defined(n)?n:new p};var m=new a.Cartesian3(-.5,-.5,0),f=new a.Cartesian3(.5,.5,0);return p.createGeometry=function(){var e=new y.GeometryAttributes,t=new Uint16Array(8),n=new Float64Array(12);return n[0]=m.x,n[1]=m.y,n[2]=m.z,n[3]=f.x,n[4]=m.y,n[5]=m.z,n[6]=f.x,n[7]=f.y,n[8]=m.z,n[9]=m.x,n[10]=f.y,n[11]=m.z,e.position=new s.GeometryAttribute({componentDatatype:u.ComponentDatatype.DOUBLE,componentsPerAttribute:3,values:n}),t[0]=0,t[1]=1,t[2]=1,t[3]=2,t[4]=2,t[5]=3,t[6]=3,t[7]=0,new s.Geometry({attributes:e,indices:t,primitiveType:s.PrimitiveType.LINES,boundingSphere:new i.BoundingSphere(a.Cartesian3.ZERO,Math.sqrt(2))})},function(e,t){return r.defined(t)&&(e=p.unpack(e,t)),p.createGeometry(e)}});
