1cb611db219934d7e5716892ca442917b5833359
Michael Fink <michael.fink@asamnet.de>
2020-02-06 07:28:25 +0100

    whitespace cleanup

--- src/App/iOS/AppDelegate.cs
+++ src/App/iOS/AppDelegate.cs
@@ -13,15 +13,15 @@ using Xamarin.Forms;
 namespace WhereToFly.App.iOS
 {
     /// <summary>
-    /// The UIApplicationDelegate for the application. This class is responsible for launching the 
-    /// User Interface of the application, as well as listening (and optionally responding) to 
+    /// The UIApplicationDelegate for the application. This class is responsible for launching the
+    /// User Interface of the application, as well as listening (and optionally responding) to
     /// application events from iOS.
     /// </summary>
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
         /// <summary>
-        /// This method is invoked when the application has loaded and is ready to run. In this 
+        /// This method is invoked when the application has loaded and is ready to run. In this
         /// method you should instantiate the window, load the UI into it and then make the window
         /// visible.
         /// You have 17 seconds to return from this method, or iOS will terminate your application.
