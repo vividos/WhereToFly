301940c81c65af35405dff1f3b7f112de9ca0cc8
Michael Fink <michael.fink@asamnet.de>
2020-02-07 08:15:43 +0100

    added enum TakeoffDirection

---
+++ src/App/Model/TakeoffDirection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace WhereToFly.App.Model
+{
+    /// <summary>
+    /// Takeoff direction as flags that can be combined. Used to mark takeoff locations with the
+    /// actual takeoff directions possible.
+    /// </summary>
+    [Flags]
+    [SuppressMessage(
+        "StyleCop.CSharp.DocumentationRules",
+        "SA1602:EnumerationItemsMustBeDocumented",
+        Justification = "No need to document the obvious cardinal directions")]
+    public enum TakeoffDirection
+    {
+        /// <summary>
+        /// No takeoff direction, none known, or no takeoff location
+        /// </summary>
+        None = 0,
+
+        N = 1 << 1,
+        NNE = 1 << 2,
+        NE = 1 << 3,
+        ENE = 1 << 4,
+        E = 1 << 5,
+        ESE = 1 << 6,
+        SE = 1 << 7,
+        SSE = 1 << 8,
+        S = 1 << 9,
+        SSW = 1 << 10,
+        SW = 1 << 11,
+        WSW = 1 << 12,
+        W = 1 << 13,
+        WNW = 1 << 14,
+        NW = 1 << 15,
+        NNW = 1 << 16
+    }
+}
