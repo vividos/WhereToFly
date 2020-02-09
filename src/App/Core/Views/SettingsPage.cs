c1da75f88b92bf1f532f6599d26f258cfe54733f
Michael Fink <michael.fink@asamnet.de>
2020-02-09 20:08:17 +0100

    store last shown settings page in AppSettings

--- src/App/Core/Views/SettingsPage.cs
+++ src/App/Core/Views/SettingsPage.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace WhereToFly.App.Core.Views
 {
@@ -16,6 +18,35 @@ namespace WhereToFly.App.Core.Views
 
             this.Children.Add(new GeneralSettingsPage());
             this.Children.Add(new MapSettingsPage());
+
+            var currentPageIndex = App.Settings.LastShownSettingsPage;
+
+            if (currentPageIndex < 0 || currentPageIndex >= this.Children.Count)
+            {
+                currentPageIndex = 0;
+            }
+
+            this.CurrentPage = this.Children[currentPageIndex];
+
+            this.CurrentPageChanged += async (sender, args) => await this.OnCurrentSettingsPageChanged();
+        }
+
+        /// <summary>
+        /// Called when the currently shwon page has changed
+        /// </summary>
+        /// <returns>task to wait on</returns>
+        protected async Task OnCurrentSettingsPageChanged()
+        {
+            int currentPageIndex = this.Children.IndexOf(this.CurrentPage);
+            if (currentPageIndex == -1)
+            {
+                return;
+            }
+
+            App.Settings.LastShownSettingsPage = currentPageIndex;
+
+            var dataService = DependencyService.Get<IDataService>();
+            await dataService.StoreAppSettingsAsync(App.Settings);
         }
     }
 }
