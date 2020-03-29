5f04c98f91c7e91f967213c722c053607831c250
Michael Fink <michael.fink@asamnet.de>
2020-03-29 22:13:25 +0200

    added initial HeightProfileView C# and JavaScript class

---
+++ src/App/Core/Views/HeightProfileView.cs
@@ -0,0 +1,137 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using WhereToFly.App.Geo;
+using Xamarin.Forms;
+
+namespace WhereToFly.App.Core.Views
+{
+    /// <summary>
+    /// View that shows a track height profile
+    /// </summary>
+    public class HeightProfileView
+    {
+        /// <summary>
+        /// Web view where HeightProfileView control is used
+        /// </summary>
+        private readonly WebView webView;
+
+        /// <summary>
+        /// Creates a new HeightProfileView C# object
+        /// </summary>
+        /// <param name="webView">web view to use</param>
+        public HeightProfileView(WebView webView)
+        {
+            this.webView = webView;
+
+            this.webView.Navigating += this.OnNavigating_WebView;
+
+            string js = "heightProfileView = new HeightProfileView({id: 'chartElement'});";
+            this.RunJavaScript(js);
+        }
+
+        /// <summary>
+        /// Sets track to display height profile
+        /// </summary>
+        /// <param name="track">track to use</param>
+        public void SetTrack(Track track)
+        {
+            var trackPointsList =
+                track.TrackPoints.SelectMany(x => new double[]
+                {
+                    x.Longitude,
+                    x.Latitude,
+                    x.Altitude ?? 0.0
+                });
+
+            List<double> timePointsList = null;
+
+            var firstTrackPoint = track.TrackPoints.FirstOrDefault();
+            if (firstTrackPoint != null &&
+                firstTrackPoint.Time.HasValue)
+            {
+                var startTime = firstTrackPoint.Time.Value;
+
+                timePointsList = track.TrackPoints.Select(x => (x.Time.Value - startTime).TotalSeconds).ToList();
+            }
+
+            var trackJsonObject = new
+            {
+                id = track.Id,
+                listOfTrackPoints = trackPointsList,
+                listOfTimePoints = timePointsList,
+            };
+
+            string js = $"heightProfileView.setTrack({JsonConvert.SerializeObject(trackJsonObject)});";
+
+            this.RunJavaScript(js);
+        }
+
+        /// <summary>
+        /// Adds a ground profile using given elevations. The number of elevation values must
+        /// match the number of track points from the SetTrack() call.
+        /// </summary>
+        /// <param name="elevationValues">elevation values</param>
+        public void AddGroundProfile(double[] elevationValues)
+        {
+            string elevations = JsonConvert.SerializeObject(elevationValues);
+            string js = $"heightProfileView.addGroundProfile({elevations});";
+
+            this.RunJavaScript(js);
+        }
+
+        /// <summary>
+        /// Called when web view navigates to a new URL; used to bypass callback:// URLs.
+        /// </summary>
+        /// <param name="sender">sender object</param>
+        /// <param name="args">event args</param>
+        private void OnNavigating_WebView(object sender, WebNavigatingEventArgs args)
+        {
+            const string CallbackSchema = "callback://";
+            if (args.Url.ToString().StartsWith(CallbackSchema))
+            {
+                args.Cancel = true;
+
+                string callbackParams = args.Url.ToString().Substring(CallbackSchema.Length);
+
+                int pos = callbackParams.IndexOf('/');
+                Debug.Assert(pos > 0, "callback Uri must contain a slash after the function name");
+
+                string functionName = callbackParams.Substring(0, pos);
+                string jsonParameters = callbackParams.Substring(pos + 1);
+
+                jsonParameters = System.Net.WebUtility.UrlDecode(jsonParameters);
+
+                this.ExecuteCallback(functionName, jsonParameters);
+            }
+        }
+
+        /// <summary>
+        /// Executes callback function
+        /// </summary>
+        /// <param name="functionName">function name of function to execute</param>
+        /// <param name="jsonParameters">JSON formatted parameters for function</param>
+        private void ExecuteCallback(string functionName, string jsonParameters)
+        {
+            switch (functionName)
+            {
+                default:
+                    Debug.Assert(false, "invalid callback function name");
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Runs JavaScript code, in main thread
+        /// </summary>
+        /// <param name="js">javascript code snippet</param>
+        private void RunJavaScript(string js)
+        {
+            Debug.WriteLine("run js: " + js.Substring(0, Math.Min(80, js.Length)));
+
+            Device.BeginInvokeOnMainThread(() => this.webView.Eval(js));
+        }
+    }
+}
---
+++ src/App/Resources/Assets/map/heightProfileView.js
@@ -0,0 +1,118 @@
+/**
+ * Creates a new instance of HeightProfileView
+ * @constructor
+ * @param {object} [options] Options to use for initializing height profile view
+ * @param {Number} [options.id] DOM ID of the canvas element to create height profile view in
+ */
+function HeightProfileView(options) {
+
+    console.log("creating new height profile view");
+
+    this.options = options || {
+        id: 'chartElement',
+        callback: {}
+    };
+
+    if (this.options.callback === undefined)
+        this.options.callback = callAction;
+
+    var ctx = document.getElementById(this.options.id).getContext('2d');
+
+    this.chart = new Chart(ctx, {
+        type: 'line',
+        data: {},
+        options: {
+            legend: {
+                display: false
+            },
+            scales: {
+                xAxes: [{
+                    id: 'time',
+                    type: 'time',
+                    time: {
+                        unit: 'hour',
+                        displayFormats: 'HH:mm',
+                        stepSize: 1,
+                        minUnit: 'second'
+                    }
+                }
+                ],
+                yAxes: [{
+                    id: 'elevation',
+                    type: 'linear',
+                    position: 'left',
+                    offset: true,
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
+
+/**
+ * Sets track to display the height profile for
+ * @param {object} [track] Track object to add
+ * @param {string} [track.id] unique ID of the track
+ * @param {string} [track.name] track name to add
+ * @param {array} [track.listOfTrackPoints] An array of track points in long, lat, alt, long, lat, alt ... order
+ * @param {array} [track.listOfTimePoints] An array of time points in seconds; same length as listOfTrackPoints; may be null
+ */
+HeightProfileView.prototype.setTrack = function (track) {
+
+    console.log("adding list of track points, with ID " + track.id + " and " + track.listOfTrackPoints.length / 3 + " track points");
+
+    var trackData = [];
+
+    for (var i = 0, len = track.listOfTrackPoints.length; i < len; i += 3) {
+        var timePoint = track.listOfTimePoints[i / 3];
+        trackData.push({
+            x: timePoint,
+            y: track.listOfTrackPoints[i + 2],
+        });
+    }
+
+    this.chart.data = {
+        datasets: [{
+            data: trackData,
+            fill: false,
+            label: 'Track',
+            tension: 0.0,
+            pointRadius: 0.0,
+            borderColor: 'rgba(0,0,255,255)',
+        }]
+    };
+
+    this.chart.update();
+};
+
+/**
+ * adds a ground profile for an already added track
+ * @param {object} [elevationArray] elevations to add
+ */
+HeightProfileView.prototype.addGroundProfile = function (elevationArray) {
+
+    console.log("adding ground profile with " + elevationArray.length + " elevation points");
+
+    var trackData = this.chart.data.datasets[0].data;
+    var elevationData = [];
+    for (var i = 0, len = trackData.length; i < len; i++) {
+        elevationData.push({
+            x: trackData[i].x,
+            y: elevationArray[i]
+        });
+    }
+
+    this.chart.data.datasets.push({
+        data: elevationData,
+        showLine: true,
+        fill: true,
+        label: 'Terrain',
+        tension: 0.0,
+        pointRadius: 0.0,
+        bckgroundColor: 'rgba(128,128,128,0)',
+    });
+
+    this.chart.update();
+};
