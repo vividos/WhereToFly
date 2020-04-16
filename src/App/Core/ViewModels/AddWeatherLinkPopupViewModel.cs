e00dbed1dedd82dede49ed4ecf0bfa21895684ed
Michael Fink <michael.fink@asamnet.de>
2020-04-16 21:49:43 +0200

    added popup page to add a new weather web link

---
+++ src/App/Core/ViewModels/AddWeatherLinkPopupViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WhereToFly.App.Model;
+
+namespace WhereToFly.App.Core.ViewModels
+{
+    /// <summary>
+    /// View model for "add weather link" popup page
+    /// </summary>
+    public class AddWeatherLinkPopupViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Weather icon description being edited
+        /// </summary>
+        public WeatherIconDescription WeatherIconDescription { get; set; }
+
+        #region Binding properties
+        /// <summary>
+        /// Weather icon description name
+        /// </summary>
+        public string Name
+        {
+            get => this.WeatherIconDescription.Name;
+            set => this.WeatherIconDescription.Name = value;
+        }
+
+        /// <summary>
+        /// Web link to edit
+        /// </summary>
+        public string WebLink
+        {
+            get => this.WeatherIconDescription.WebLink;
+            set
+            {
+                this.WeatherIconDescription.WebLink = value;
+                this.OnPropertyChanged(nameof(this.IsValidWebLink));
+            }
+        }
+
+        /// <summary>
+        /// List of all groups to select from
+        /// </summary>
+        public IEnumerable<string> GroupsList { get; }
+
+        /// <summary>
+        /// Currently selected group
+        /// </summary>
+        public string SelectedGroup
+        {
+            get => this.WeatherIconDescription.Group;
+            set => this.WeatherIconDescription.Group = value;
+        }
+
+        /// <summary>
+        /// Returns if the currently input web link is valid
+        /// </summary>
+        public bool IsValidWebLink
+        {
+            get => Uri.TryCreate(this.WebLink, UriKind.Absolute, out Uri _);
+        }
+        #endregion
+
+        /// <summary>
+        /// Creates a new view model for the "add weather link" page
+        /// </summary>
+        public AddWeatherLinkPopupViewModel()
+        {
+            this.GroupsList = new List<string>
+            {
+                "Weather forecast",
+                "Current weather",
+                "Webcams",
+            };
+
+            this.WeatherIconDescription = new WeatherIconDescription
+            {
+                Name = string.Empty,
+                Group = this.GroupsList.First(),
+                Type = WeatherIconDescription.IconType.IconLink,
+                WebLink = string.Empty
+            };
+        }
+    }
+}
---
+++ src/App/Core/Views/AddWeatherLinkPopupPage.xaml.cs
@@ -0,0 +1,96 @@
+﻿using Rg.Plugins.Popup.Extensions;
+using Rg.Plugins.Popup.Pages;
+using System;
+using System.Threading.Tasks;
+using WhereToFly.App.Core.ViewModels;
+using WhereToFly.App.Model;
+
+namespace WhereToFly.App.Core.Views
+{
+    /// <summary>
+    /// Popup page for adding a new weather link.
+    /// </summary>
+    public partial class AddWeatherLinkPopupPage : PopupPage
+    {
+        /// <summary>
+        /// View model for this popup page
+        /// </summary>
+        private readonly AddWeatherLinkPopupViewModel viewModel;
+
+        /// <summary>
+        /// Task completion source to report back weather icon description
+        /// </summary>
+        private TaskCompletionSource<WeatherIconDescription> tcs;
+
+        /// <summary>
+        /// Creates a new popup page to add weather link
+        /// </summary>
+        public AddWeatherLinkPopupPage()
+        {
+            this.CloseWhenBackgroundIsClicked = true;
+
+            this.InitializeComponent();
+
+            this.BindingContext = this.viewModel = new AddWeatherLinkPopupViewModel();
+        }
+
+        /// <summary>
+        /// Shows "add weather link" popup page and lets the user type or paste a link.
+        /// </summary>
+        /// <returns>weather icon description, or null when the popup page was cancelled</returns>
+        public static async Task<WeatherIconDescription> ShowAsync()
+        {
+            var popupPage = new AddWeatherLinkPopupPage
+            {
+                tcs = new TaskCompletionSource<WeatherIconDescription>()
+            };
+
+            await popupPage.Navigation.PushPopupAsync(popupPage);
+
+            return await popupPage.tcs.Task;
+        }
+
+        /// <summary>
+        /// Called when user clicked on the background, dismissing the popup page.
+        /// </summary>
+        /// <returns>whatever the base class returns</returns>
+        protected override bool OnBackgroundClicked()
+        {
+            if (!this.tcs.Task.IsCompleted)
+            {
+                this.tcs.SetResult(null);
+            }
+
+            return base.OnBackgroundClicked();
+        }
+
+        /// <summary>
+        /// Called when user naviaged back with the back button, dismissing the popup page.
+        /// </summary>
+        /// <returns>whatever the base class returns</returns>
+        protected override bool OnBackButtonPressed()
+        {
+            if (!this.tcs.Task.IsCompleted)
+            {
+                this.tcs.SetResult(null);
+            }
+
+            return base.OnBackButtonPressed();
+        }
+
+        /// <summary>
+        /// Called when user clicked on the "Add weather link" button, ending the popup page.
+        /// </summary>
+        /// <param name="sender">sender object</param>
+        /// <param name="args">event args</param>
+        private async void OnClickedAddWeatherLinkButton(object sender, EventArgs args)
+        {
+            if (!this.tcs.Task.IsCompleted)
+            {
+                this.tcs.SetResult(this.viewModel.WeatherIconDescription);
+            }
+
+            await this.Navigation.PopPopupAsync();
+        }
+    }
+}
