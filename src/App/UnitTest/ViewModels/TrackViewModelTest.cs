facc57e124052b6ac6a99e8a2c1464ed5b4d90dd
Michael Fink <michael.fink@asamnet.de>
2020-05-10 22:21:17 +0200

    fixed unit tests for track view models

--- src/App/UnitTest/ViewModels/TrackViewModelTest.cs
+++ src/App/UnitTest/ViewModels/TrackViewModelTest.cs
@@ -8,10 +8,11 @@ using Xamarin.Forms;
 namespace WhereToFly.App.UnitTest.ViewModels
 {
     /// <summary>
-    /// Unit tests for class TrackDetailsViewModel
+    /// Unit tests for view model classs TrackDetailsViewModel, TrackStatisticsViewModel and
+    /// TrackTabViewModel.
     /// </summary>
     [TestClass]
-    public class TrackDetailsViewModelTest
+    public class TrackViewModelTest
     {
         /// <summary>
         /// Sets up tests by initializing Xamarin.Forms.Mocks and DependencyService with
@@ -30,10 +31,31 @@ namespace WhereToFly.App.UnitTest.ViewModels
         }
 
         /// <summary>
-        /// Tests default ctor of view model
+        /// Tests default ctor of TrackTabViewModel
         /// </summary>
         [TestMethod]
-        public void TestDefaultCtor()
+        public void TestTrackTabViewModel()
+        {
+            // set up
+            var track = UnitTestHelper.GetDefaultTrack();
+            track.CalculateStatistics();
+
+            // run
+            var viewModel = new TrackTabViewModel(track);
+
+            // check
+            Assert.IsNotNull(viewModel.ZoomToTrackCommand, "zoom command must not be null");
+            Assert.IsNotNull(viewModel.DeleteTrackCommand, "delete command must not be null");
+
+            viewModel.ZoomToTrackCommand.Execute(null);
+            viewModel.DeleteTrackCommand.Execute(null);
+        }
+
+        /// <summary>
+        /// Tests default ctor of TrackDetailsViewModel
+        /// </summary>
+        [TestMethod]
+        public void TestTrackDetailsViewModel()
         {
             // set up
             var track = UnitTestHelper.GetDefaultTrack();
@@ -44,6 +66,23 @@ namespace WhereToFly.App.UnitTest.ViewModels
 
             // check
             Assert.IsTrue(viewModel.Name.Any(), "name must contain value");
+            Assert.IsNotNull(viewModel.TypeImageSource, "type image source must not be null");
+        }
+
+        /// <summary>
+        /// Tests default ctor of TrackStatisticsViewModel
+        /// </summary>
+        [TestMethod]
+        public void TestDefaultCtor()
+        {
+            // set up
+            var track = UnitTestHelper.GetDefaultTrack();
+            track.CalculateStatistics();
+
+            // run
+            var viewModel = new TrackStatisticsViewModel(track);
+
+            // check
             Assert.IsTrue(viewModel.NumTrackPoints > 0, "there must be some track points");
             Assert.IsTrue(viewModel.Distance.Any(), "distance must contain value");
             Assert.IsTrue(viewModel.Duration.Any(), "duration must contain value");
@@ -56,13 +95,6 @@ namespace WhereToFly.App.UnitTest.ViewModels
             Assert.IsTrue(viewModel.MaxSinkRate.Any(), "max. sink rate must contain value");
             Assert.IsTrue(viewModel.MaxSpeed.Any(), "max. speed must contain value");
             Assert.IsTrue(viewModel.AverageSpeed.Any(), "average speed must contain value");
-
-            Assert.IsNotNull(viewModel.ZoomToTrackCommand, "zoom command must not be null");
-            Assert.IsNotNull(viewModel.DeleteTrackCommand, "delete command must not be null");
-            Assert.IsNotNull(viewModel.TypeImageSource, "type image source must not be null");
-
-            viewModel.ZoomToTrackCommand.Execute(null);
-            viewModel.DeleteTrackCommand.Execute(null);
         }
     }
 }
