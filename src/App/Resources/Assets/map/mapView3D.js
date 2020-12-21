eaaae46837ae88a4b016b9c140e3f815027e2ae7
Michael Fink <michael.fink@asamnet.de>
2020-12-21 17:06:12 +0100

    fixed track marker image path

--- src/App/Resources/Assets/map/mapView3D.js
+++ src/App/Resources/Assets/map/mapView3D.js
@@ -198,7 +198,7 @@ function MapView(options) {
 
     // the track marker is initially invisible and only used when a track is tapped
     Cesium.when(
-        this.createEntity('trackMarker', 'Track point', '', Cesium.Color.PURPLE, '../images/map-marker-distance.svg', 0.0, 0.0),
+        this.createEntity('trackMarker', 'Track point', '', Cesium.Color.PURPLE, 'images/map-marker-distance.svg', 0.0, 0.0),
         function (trackEntity) {
             trackEntity.show = false;
             trackEntity.billboard.heightReference = Cesium.HeightReference.NONE;
