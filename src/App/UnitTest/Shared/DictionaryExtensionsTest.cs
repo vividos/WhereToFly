f9fc15f475921177cdae266764643e59bf2df65b
Michael Fink <michael.fink@asamnet.de>
2021-01-22 18:36:14 +0100

    added some unit tests for the WhereToFly.Shared.Base assembly

---
+++ src/App/UnitTest/Shared/DictionaryExtensionsTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using WhereToFly.Shared.Base;
+
+namespace WhereToFly.App.UnitTest.Shared
+{
+    /// <summary>
+    /// Tests for the DictionaryExtensions extension methods
+    /// </summary>
+    [TestClass]
+    public class DictionaryExtensionsTest
+    {
+        /// <summary>
+        /// Tests extension method GetValueOrDefault().
+        /// </summary>
+        [TestMethod]
+        public void TestGetValueOrDefault()
+        {
+            // set up
+            var dict = new Dictionary<string, int>();
+            dict.Add("key1", 42);
+
+            Dictionary<string, int> nullDict = null;
+
+            // run + check
+            int value1 = dict.GetValueOrDefault("key1", 64);
+            Assert.AreEqual(42, value1, "value must match value in dictionary");
+
+            int value2 = dict.GetValueOrDefault("key2", 64);
+            Assert.AreEqual(64, value2, "value must match default value");
+
+            Assert.ThrowsException<ArgumentNullException>(
+                () => nullDict.GetValueOrDefault("key3", 128),
+                "must throw eception when dict is null");
+        }
+    }
+}
---
+++ src/App/UnitTest/Shared/MathTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhereToFly.Shared.Base;
+
+namespace WhereToFly.App.UnitTest.Shared
+{
+    /// <summary>
+    /// Tests for the Math methods in WhereToFly.Shared.Base
+    /// </summary>
+    [TestClass]
+    public class MathTest
+    {
+        /// <summary>
+        /// Tests method Interpolate().
+        /// </summary>
+        [TestMethod]
+        public void TestInterpolate()
+        {
+            // check
+            var value1 = Math.Interpolate(0.0, 10.0, 0.42);
+            var value2 = Math.Interpolate(0.0, 1.0, -2);
+            var value3 = Math.Interpolate(10.0, 0.0, 0.99);
+
+            // check
+            Assert.AreEqual(4.2, value1, "interpolated value must match");
+            Assert.AreEqual(-2.0, value2, "interpolated value must match");
+            Assert.AreEqual(0.1, value3, "interpolated value must match");
+        }
+    }
+}
