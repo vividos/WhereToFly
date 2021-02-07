38edd06fc5722199f1b637b0ee2b2a479fa43c08
Michael Fink <michael.fink@asamnet.de>
2021-02-07 16:59:47 +0100

    converted method to calculate location bounding rectangle to extension method

--- src/App/Core/OpenFileHelper.cs
+++ src/App/Core/OpenFileHelper.cs
@@ -323,30 +323,7 @@ namespace WhereToFly.App.Core
         /// <param name="locationList">list of all locations</param>
         private static void ZoomToLocationList(List<Location> locationList)
         {
-            const double defaultMinAltitudeValue = 10000.0;
-            const double defaultMaxAltitudeValue = -10000.0;
-            var minLocation = new MapPoint(90.0, 360.0, defaultMinAltitudeValue);
-            var maxLocation = new MapPoint(-90.0, -360.0, defaultMaxAltitudeValue);
-
-            foreach (var location in locationList)
-            {
-                minLocation.Latitude = Math.Min(location.MapLocation.Latitude, minLocation.Latitude);
-                maxLocation.Latitude = Math.Max(location.MapLocation.Latitude, maxLocation.Latitude);
-                minLocation.Longitude = Math.Min(location.MapLocation.Longitude, minLocation.Longitude);
-                maxLocation.Longitude = Math.Max(location.MapLocation.Longitude, maxLocation.Longitude);
-                minLocation.Altitude = Math.Min(location.MapLocation.Altitude ?? defaultMinAltitudeValue, minLocation.Altitude.Value);
-                maxLocation.Altitude = Math.Max(location.MapLocation.Altitude ?? defaultMaxAltitudeValue, maxLocation.Altitude.Value);
-            }
-
-            if (Math.Abs(minLocation.Altitude.Value - defaultMinAltitudeValue) < 1e-6)
-            {
-                minLocation.Altitude = 0.0;
-            }
-
-            if (Math.Abs(maxLocation.Altitude.Value - defaultMaxAltitudeValue) < 1e-6)
-            {
-                maxLocation.Altitude = 0.0;
-            }
+            locationList.GetBoundingRectangle(out MapPoint minLocation, out MapPoint maxLocation);
 
             App.MapView.ZoomToRectangle(minLocation, maxLocation);
         }
