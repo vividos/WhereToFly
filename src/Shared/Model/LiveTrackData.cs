7935b9784fcfda3a85f36820b22b2625a740ddfe
Michael Fink <michael.fink@asamnet.de>
2021-06-20 14:27:40 +0200

    added model classes for live tracking webapi

---
+++ src/Shared/Model/LiveTrackData.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace WhereToFly.Shared.Model
+{
+    /// <summary>
+    /// Data for a Live Track
+    /// </summary>
+    public class LiveTrackData
+    {
+        /// <summary>
+        /// Live Track ID
+        /// </summary>
+        public string ID { get; set; }
+
+        /// <summary>
+        /// Name of live waypoint
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Detailed description, in MarkDown format
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Track starting point
+        /// </summary>
+        public DateTimeOffset TrackStart { get; set; }
+
+        /// <summary>
+        /// Live tracking track point
+        /// </summary>
+        public struct LiveTrackPoint
+        {
+            /// <summary>
+            /// Offset of track point to track start
+            /// </summary>
+            public double Offset { get; set; }
+
+            /// <summary>
+            /// Latitude value, positive values to the north
+            /// </summary>
+            public double Latitude { get; set; }
+
+            /// <summary>
+            /// Longitude value, positive values to the east
+            /// </summary>
+            public double Longitude { get; set; }
+
+            /// <summary>
+            /// Altitude value, if available; 0 means "clamp to ground"
+            /// </summary>
+            public double Altitude { get; set; }
+        }
+
+        /// <summary>
+        /// List of all track points
+        /// </summary>
+        public LiveTrackPoint[] TrackPoints { get; set; } = Array.Empty<LiveTrackPoint>();
+    }
+}
---
+++ src/Shared/Model/LiveTrackQueryResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WhereToFly.Shared.Model
+{
+    /// <summary>
+    /// Query result for live track data
+    /// </summary>
+    [Serializable]
+    public class LiveTrackQueryResult
+    {
+        /// <summary>
+        /// Live track data; always set, but may contain zero live track points
+        /// </summary>
+        public LiveTrackData Data { get; set; }
+
+        /// <summary>
+        /// Date where next request for the requested live track will be most probably
+        /// available
+        /// </summary>
+        public DateTimeOffset NextRequestDate { get; set; }
+    }
+}
