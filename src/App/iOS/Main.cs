a43d65416978943b358085406eac3f0590eb74b5
Michael Fink <michael.fink@asamnet.de>
2021-11-05 23:26:30 +0100

    fixed warning in iOS main method

--- src/App/iOS/Main.cs
+++ src/App/iOS/Main.cs
@@ -26,7 +26,7 @@ namespace WhereToFly.App.iOS
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
-            UIApplication.Main(args, null, "AppDelegate");
+            UIApplication.Main(args, null, typeof(AppDelegate));
         }
     }
 }
