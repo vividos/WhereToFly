a76396cb91555c24db2c8544e5d636d150f17a1b
Michael Fink <michael.fink@asamnet.de>
2021-11-07 11:04:54 +0100

    removed iOS project, as it never really worked

--- src/App/iOS/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("WhereToFly.App.iOS")]
-[assembly: AssemblyDescription("Where-to-fly iOS app")]
-[assembly: AssemblyConfiguration("ARM64")]
-
-[assembly: ComVisible(false)]
