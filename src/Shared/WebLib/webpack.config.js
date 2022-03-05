4425d8eb64d9d98934f09695f6ca9d517106ba37
Michael Fink <michael.fink@asamnet.de>
2022-03-05 17:37:06 +0100

    added initial WebLib javascript npm project and msbuild project

---
+++ src/Shared/WebLib/webpack.config.js
@@ -0,0 +1,32 @@
+﻿const path = require('path');
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: "./src/js/index.js",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/WhereToFly.[name].js',
+        library: ["WhereToFly", "[name]"],
+        libraryTarget: "umd",
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [{ loader: MiniCssExtractPlugin.loader }, "css-loader"],
+                sideEffects: true
+            }, {
+                test: /\.(png|gif|jpg|jpeg|svg|xml|json|czml)$/,
+                use: ['url-loader']
+            },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/[name].css"
+        }),
+    ],
+};
---
+++ src/Shared/WebLib/webpack.prod.config.js
@@ -0,0 +1,43 @@
+﻿const path = require('path');
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+    mode: 'production',
+    entry: "./src/js/index.js",
+    output: {
+        clean: true,
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/WhereToFly.[name].js',
+        library: ["WhereToFly", "[name]"],
+        libraryTarget: "umd",
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [{ loader: MiniCssExtractPlugin.loader }, "css-loader"],
+                sideEffects: true
+            }, {
+                test: /\.(png|gif|jpg|jpeg|svg|xml|json|czml)$/,
+                use: ['url-loader']
+            },
+        ]
+    },
+    optimization: {
+        usedExports: true,
+        minimize: true,
+        minimizer: [
+            new TerserPlugin(),
+            new CssMinimizerPlugin(),
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/[name].css"
+        }),
+    ],
+};
