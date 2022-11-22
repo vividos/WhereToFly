c4cde538a72aaeff6f4b2ae5e6a2314544b5d6a2
Michael Fink <michael.fink@asamnet.de>
2022-11-22 22:24:58 +0100

    added INearbyPoiService interface

---
+++ src/App/MapView/INearbyPoiService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WhereToFly.Geo.Model;
+
+namespace WhereToFly.App.MapView
+{
+    /// <summary>
+    /// Service to find and access nearby POIs
+    /// </summary>
+    public interface INearbyPoiService
+    {
+        /// <summary>
+        /// Returns list of nearby POIs in the given map area
+        /// </summary>
+        /// <param name="area">map area to find POIs in</param>
+        /// <param name="visiblePoiIds">list of POIs already visible in the area</param>
+        /// <returns>list of new locations</returns>
+        Task<IEnumerable<Location>> Get(MapRectangle area, IEnumerable<string> visiblePoiIds);
+    }
+}
