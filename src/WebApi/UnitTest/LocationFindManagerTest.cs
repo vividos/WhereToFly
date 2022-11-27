c9372cff0857d1a4002b69b62e1181840f6d425d
Michael Fink <michael.fink@asamnet.de>
2022-11-27 15:37:42 +0100

    added another test for LocationFindManager

--- src/WebApi/UnitTest/LocationFindManagerTest.cs
+++ src/WebApi/UnitTest/LocationFindManagerTest.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using WhereToFly.Geo.Model;
 using WhereToFly.WebApi.Logic;
@@ -26,6 +29,54 @@ namespace WhereToFly.WebApi.UnitTest
 
             // check
             Assert.IsNull(location, "location must not exist");
+        }
+
+        /// <summary>
+        /// Tests timing of querying all latitude/longitude combinations
+        /// </summary>
+        /// <returns>task to wait on</returns>
+        [TestMethod]
+        public async Task TestTimingAllLatLong()
+        {
+            // set up
+            var mgr = new LocationFindManager();
+            var stopwatch = new Stopwatch();
+
+            // run
+            ////for (int latitude = -90; latitude < 90; latitude++)
+            for (int latitude = 40; latitude < 50; latitude++)
+            {
+                int maxNumberOfLocations = 0;
+                double maxTime = 0.0;
+
+                for (int longitude = 0; longitude < 360; longitude++)
+                {
+                    stopwatch.Start();
+
+                    var results = await mgr.FindAsync(
+                        new MapPoint(latitude + 0.5, longitude + 0.5),
+                        rangeInMeter: 50 * 1000.0);
+
+                    int numberOfLocations = results.Count();
+
+                    stopwatch.Stop();
+
+                    double time = stopwatch.Elapsed.TotalSeconds;
+
+                    maxNumberOfLocations = Math.Max(maxNumberOfLocations, numberOfLocations);
+                    maxTime = Math.Max(maxTime, time);
+
+                    Debug.WriteLine($"{latitude}/{longitude}: {numberOfLocations} locations in {time} seconds");
+
+                    stopwatch.Reset();
+                }
+
+                Debug.WriteLine($"{latitude}: {maxNumberOfLocations} locations in {maxTime} seconds");
+
+                Assert.IsTrue(
+                    maxNumberOfLocations > 0,
+                    "there must be locations anywhere in the checked range");
+            }
         }
     }
 }
