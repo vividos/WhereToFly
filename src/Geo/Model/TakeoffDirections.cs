3145d0d0ba82505ee38fb2e17613236fece660b1
Michael Fink <michael.fink@asamnet.de>
2022-11-27 12:33:22 +0100

    suppress warning "CS1591: Missing XML comment for publicly visible type or member" for enum TakeoffDirections

--- src/Geo/Model/TakeoffDirections.cs
+++ src/Geo/Model/TakeoffDirections.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
 
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
 namespace WhereToFly.Geo.Model
 {
     /// <summary>
