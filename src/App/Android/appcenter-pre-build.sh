55da2cbd33cf356a6034210f7f9e97f46e9a6f84
Michael Fink <michael.fink@asamnet.de>
2022-11-27 13:24:45 +0100

    try the latest Mono preview

--- src/App/Android/appcenter-pre-build.sh
+++ src/App/Android/appcenter-pre-build.sh
@@ -1,10 +1,12 @@
 ﻿#!/usr/bin/env bash
 # App Center custom build scripts: https://aka.ms/docs/build/custom/scripts
 
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
+export DOTNET_NOLOGO=true
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
 
 # Install Xamarin.Android using Boots global tool
 dotnet tool install --global boots
 
 boots https://aka.ms/xamarin-android-commercial-d17-4-macos
-boots --stable Mono
+boots --preview Mono
