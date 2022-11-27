96d2abe296848287c1b3ed14dc3bdff0b852d725
Michael Fink <michael.fink@asamnet.de>
2022-11-27 12:52:36 +0100

    try to fix AppCenter Android build by using pre build script
    reference: https://github.com/jonathanpeppers/boots/issues/90

---
+++ src/App/Android/appcenter-pre-build.sh
@@ -0,0 +1,9 @@
+﻿#!/usr/bin/env bash
+# App Center custom build scripts: https://aka.ms/docs/build/custom/scripts
+
+export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
+
+# Install Xamarin.Android using Boots global tool
+dotnet tool install --global boots
+
+boots https://aka.ms/xamarin-android-commercial-d17-4-macos
