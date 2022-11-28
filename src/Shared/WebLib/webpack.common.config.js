2f58d1e6b2d7da5c0b281aa2ae2528ec9c9fba56
Michael Fink <michael.fink@asamnet.de>
2022-11-28 21:55:33 +0100

    weblib: limit number of chunks to 1
    fixes generating chunks for resolve.fallback rules

--- src/Shared/WebLib/webpack.common.config.js
+++ src/Shared/WebLib/webpack.common.config.js
@@ -83,6 +83,9 @@ module.exports = {
         new webpack.DefinePlugin({
             // Define relative base path in cesium for loading assets
             CESIUM_BASE_URL: JSON.stringify("./js/")
+        }),
+        new webpack.optimize.LimitChunkCountPlugin({
+            maxChunks: 1
         })
     ],
     amd: {
