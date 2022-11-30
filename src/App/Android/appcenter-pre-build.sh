36999fb8c887632044943903c274fdd2c946de21
Michael Fink <michael.fink@asamnet.de>
2022-11-30 19:36:17 +0100

    appcenter: build the MapView project before building the Android app

--- src/App/Android/appcenter-pre-build.sh
+++ src/App/Android/appcenter-pre-build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+﻿#!/usr/bin/env bash
 # App Center custom build scripts: https://aka.ms/docs/build/custom/scripts
 
 # build the weblib npm package
@@ -6,3 +6,6 @@ pushd ../../Shared/WebLib
 npm install
 npm run build-release
 popd
+
+# also build the MapView project before building the Android app
+msbuild ../MapView/WhereToFly.App.MapView.csproj /restore /t:Build /p:Configuration=Release
