0e96b36991e29bd650ef39067dee30486e58680f
Michael Fink <michael.fink@asamnet.de>
2023-03-25 13:51:48 +0100

    added cmd script to clean temp files and folders

---
+++ src/Clean.cmd
@@ -0,0 +1,36 @@
+@echo off
+REM
+REM Where-to-fly - an app to decide where to (hike up and) fly with a paraglider
+REM Copyright (C) 2023 Michael Fink
+REM
+REM Cleans all temporary files and folders, including bin and obj filders
+REM
+echo Cleaning all temporary files and folders...
+echo.
+
+echo Removing "bin" and "obj" folders in...
+for /R %%A in (*.csproj) do (
+    echo %%~dpA
+    rmdir /S /Q %%~dpA\bin\ 2> nul
+    rmdir /S /Q %%~dpA\obj\ 2> nul
+)
+echo.
+
+echo Removing "node_modules" folder in...
+for /R %%A in (.) do if exist "%%~fA\node_modules" (
+    echo %%~fA
+    rmdir /S /Q %%~fA\node_modules\ 2> nul
+)
+echo.
+
+echo Removing npm projects' dist folders
+rmdir /S /Q Shared\WebLib\dist 2> nul
+rmdir /S /Q Web\LiveTracking\wwwroot 2> nul
+echo.
+
+echo Removing ".vs" folder in...
+for /R %%A in (.) do if exist %%~dpA\.vs (
+    echo %%~dpA
+    rmdir /S /Q %%~dpA\.vs\ 2> nul
+)
+echo.
