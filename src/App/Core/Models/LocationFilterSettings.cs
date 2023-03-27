4efd7387928c606534a2773aa0ddb3898c173c9c
Michael Fink <michael.fink@asamnet.de>
2023-03-27 22:24:35 +0200

    converted class LocationFilterSettings to record

--- src/App/Core/Models/LocationFilterSettings.cs
+++ src/App/Core/Models/LocationFilterSettings.cs
@@ -6,7 +6,7 @@ namespace WhereToFly.App.Core.Models
     /// <summary>
     /// Location filter settings
     /// </summary>
-    public sealed class LocationFilterSettings : IEquatable<LocationFilterSettings>
+    public record LocationFilterSettings
     {
         /// <summary>
         /// Last used filter text
@@ -33,58 +33,6 @@ namespace WhereToFly.App.Core.Models
         {
             return $"Text={this.FilterText}, TakeoffDirections={this.FilterTakeoffDirections}, ShowNonTakeoffLocations={this.ShowNonTakeoffLocations}";
         }
-
-        /// <summary>
-        /// Returns hash code for app resource URI
-        /// </summary>
-        /// <returns>calculated hash code</returns>
-        public override int GetHashCode() =>
-            (this.FilterText,
-            this.FilterTakeoffDirections,
-            this.ShowNonTakeoffLocations).GetHashCode();
-
-        /// <summary>
-        /// Compares this location filter settings to another object
-        /// </summary>
-        /// <param name="obj">object to compare to</param>
-        /// <returns>true when equal filter settings, false when not</returns>
-        public override bool Equals(object obj) =>
-            (obj is LocationFilterSettings filterSettings) && this.Equals(filterSettings);
         #endregion
-
-        #region IEquatable implementation
-
-        /// <summary>
-        /// Compares this app settings to another app settings object
-        /// </summary>
-        /// <param name="other">app settings object to compare to</param>
-        /// <returns>true when equal app settings, false when not</returns>
-        public bool Equals(LocationFilterSettings other) =>
-            other != null &&
-            (this.FilterText,
-            this.FilterTakeoffDirections,
-            this.ShowNonTakeoffLocations) ==
-            (other.FilterText,
-            other.FilterTakeoffDirections,
-            other.ShowNonTakeoffLocations);
-        #endregion
-
-        /// <summary>
-        /// Equality operator
-        /// </summary>
-        /// <param name="left">left operator argument</param>
-        /// <param name="right">right operator argument</param>
-        /// <returns>true when objects are equal, false when not</returns>
-        public static bool operator ==(LocationFilterSettings left, LocationFilterSettings right) =>
-            Equals(left, right);
-
-        /// <summary>
-        /// Inequality operator
-        /// </summary>
-        /// <param name="left">left operator argument</param>
-        /// <param name="right">right operator argument</param>
-        /// <returns>true when objects are inequal, false when not</returns>
-        public static bool operator !=(LocationFilterSettings left, LocationFilterSettings right) =>
-            !Equals(left, right);
     }
 }
