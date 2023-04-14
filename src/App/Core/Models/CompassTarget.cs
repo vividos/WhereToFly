04d8dd81d6a97f938908fa515f0655248e9970e0
Michael Fink <michael.fink@asamnet.de>
2023-04-14 08:07:14 +0200

    converted CompassTarget to record

--- src/App/Core/Models/CompassTarget.cs
+++ src/App/Core/Models/CompassTarget.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using WhereToFly.Geo.Model;
+﻿using WhereToFly.Geo.Model;
 
 namespace WhereToFly.App.Core.Models
 {
@@ -8,7 +7,7 @@ namespace WhereToFly.App.Core.Models
     /// current location. Note that target location overrides the target direction, if both are
     /// set.
     /// </summary>
-    public sealed class CompassTarget : IEquatable<CompassTarget>
+    public record CompassTarget
     {
         /// <summary>
         /// Title of compass target
@@ -24,60 +23,5 @@ namespace WhereToFly.App.Core.Models
         /// Target direction angle, in degrees
         /// </summary>
         public int? TargetDirection { get; set; }
-
-        #region object overridables implementation
-
-        /// <summary>
-        /// Returns hash code for this object
-        /// </summary>
-        /// <returns>calculated hash code</returns>
-        public override int GetHashCode() =>
-            (this.Title,
-            this.TargetLocation,
-            this.TargetDirection).GetHashCode();
-
-        /// <summary>
-        /// Compares this compass target to another object
-        /// </summary>
-        /// <param name="obj">object to compare to</param>
-        /// <returns>true when equal compass targets, false when not</returns>
-        public override bool Equals(object obj) =>
-            (obj is CompassTarget compassTarget) && this.Equals(compassTarget);
-        #endregion
-
-        #region IEquatable implementation
-
-        /// <summary>
-        /// Compares this compass target to another compass target object
-        /// </summary>
-        /// <param name="other">compass target object to compare to</param>
-        /// <returns>true when equal compass targets, false when not</returns>
-        public bool Equals(CompassTarget other) =>
-            other != null &&
-            (this.Title,
-            this.TargetLocation,
-            this.TargetDirection) ==
-            (other.Title,
-            other.TargetLocation,
-            other.TargetDirection);
-        #endregion
-
-        /// <summary>
-        /// Equality operator
-        /// </summary>
-        /// <param name="left">left operator argument</param>
-        /// <param name="right">right operator argument</param>
-        /// <returns>true when objects are equal, false when not</returns>
-        public static bool operator ==(CompassTarget left, CompassTarget right) =>
-            Equals(left, right);
-
-        /// <summary>
-        /// Inequality operator
-        /// </summary>
-        /// <param name="left">left operator argument</param>
-        /// <param name="right">right operator argument</param>
-        /// <returns>true when objects are inequal, false when not</returns>
-        public static bool operator !=(CompassTarget left, CompassTarget right) =>
-            !Equals(left, right);
     }
 }
