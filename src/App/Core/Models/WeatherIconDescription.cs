f5d71613d0a63cad7819e110a0d89c8faf1b9c7c
Michael Fink <michael.fink@asamnet.de>
2023-04-14 21:30:55 +0200

    converted WeatherIconDescription to record

--- src/App/Core/Models/WeatherIconDescription.cs
+++ src/App/Core/Models/WeatherIconDescription.cs
@@ -5,7 +5,7 @@ namespace WhereToFly.App.Core.Models
     /// <summary>
     /// Description for a single weather icon
     /// </summary>
-    public sealed class WeatherIconDescription : IEquatable<WeatherIconDescription>
+    public record WeatherIconDescription
     {
         /// <summary>
         /// Name of weather icon to display
@@ -47,54 +47,5 @@ namespace WhereToFly.App.Core.Models
         /// Web link or app link
         /// </summary>
         public string WebLink { get; set; } = string.Empty;
-
-        #region IEquatable implementation
-
-        /// <summary>
-        /// Compares this track with another
-        /// </summary>
-        /// <param name="other">track to compare to</param>
-        /// <returns>true when track are equal, false when not</returns>
-        public bool Equals(WeatherIconDescription other)
-        {
-            if (other == null)
-            {
-                return false;
-            }
-
-            return this.Name == other.Name &&
-                this.Group == other.Group &&
-                this.Type == other.Type &&
-                this.WebLink == other.WebLink;
-        }
-        #endregion
-
-        #region object overridables implementation
-
-        /// <summary>
-        /// Compares this weather icon description to another object
-        /// </summary>
-        /// <param name="obj">object to compare to</param>
-        /// <returns>true when weather icon descriptions are equal, false when not</returns>
-        public override bool Equals(object obj) =>
-            (obj is WeatherIconDescription weatherIcon) &&
-            this.Equals(weatherIcon);
-
-        /// <summary>
-        /// Calculates hash code for track
-        /// </summary>
-        /// <returns>calculated hash code</returns>
-        public override int GetHashCode()
-        {
-            int hashCode = 487;
-
-            hashCode = (hashCode * 31) + this.Name.GetHashCode();
-            hashCode = (hashCode * 31) + this.Group.GetHashCode();
-            hashCode = (hashCode * 31) + this.Type.GetHashCode();
-            hashCode = (hashCode * 31) + this.WebLink.GetHashCode();
-
-            return hashCode;
-        }
-        #endregion
     }
 }
