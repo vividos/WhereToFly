c33a8d4e32808eb1e016dfc5ca592131906a3527
Michael Fink <michael.fink@asamnet.de>
2023-11-09 20:34:17 +0100

    added convenient access to Assets in Resources project
    also added netstandard2.0 target framework
    and used MauiAsset in .NET 7

---
+++ src/App/Resources/Assets.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+
+namespace WhereToFly.App.Resources
+{
+    /// <summary>
+    /// Access to the resource's assets
+    /// </summary>
+    public static class Assets
+    {
+        /// <summary>
+        /// Gets an asset as Stream
+        /// </summary>
+        /// <param name="assetRelativeFilename">relative path to asset file</param>
+        /// <returns>stream, or null when asset file couldn't be found</returns>
+        public static async Task<Stream?> Get(string assetRelativeFilename)
+        {
+#if NET7_0_OR_GREATER
+            return await Microsoft.Maui.Storage.
+                FileSystem.OpenAppPackageFileAsync(assetRelativeFilename);
+#elif NETSTANDARD
+            string dottedFilename = "WhereToFly.App.Resources.Assets." +
+                assetRelativeFilename.Replace("/", ".");
+
+            return typeof(Assets).Assembly.GetManifestResourceStream(
+                dottedFilename);
+#elif MONOANDROID
+            return await Xamarin.Essentials.
+                FileSystem.OpenAppPackageFileAsync(assetRelativeFilename);
+#elif WINDOWS_UWP
+            return await Xamarin.Essentials.
+                FileSystem.OpenAppPackageFileAsync(
+                    "WhereToFly.App.Resources/Assets/" + assetRelativeFilename);
+#endif
+        }
+    }
+}
---
+++ src/App/UnitTest/AssetsTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Threading.Tasks;
+using WhereToFly.App.Resources;
+
+namespace WhereToFly.App.UnitTest
+{
+    /// <summary>
+    /// Tests class Assets from the WhereToFly.App.Resources project
+    /// </summary>
+    [TestClass]
+
+    public class AssetsTest
+    {
+        /// <summary>
+        /// Tests getting stream of assets file
+        /// </summary>
+        /// <returns>task to wait on</returns>
+        [TestMethod]
+        public async Task TestGetStream()
+        {
+            // run
+            using var stream = await Assets.Get("info/Changelog.md");
+            using var reader = new StreamReader(stream);
+            string text = await reader.ReadToEndAsync();
+
+            // check
+            Assert.IsTrue(text.Length > 0, "changelog file must have been read");
+        }
+    }
+}
