583c4dd69c3fb1f3175db5da7d174dbdf63aa00f
Michael Fink <michael.fink@asamnet.de>
2024-02-08 08:41:58 +0100

    added coordinates parser class and tests

---
+++ src/App/UnitTest/Geo/CoordinatesParserTest.cs
@@ -0,0 +1,72 @@
+﻿#nullable enable
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhereToFly.Geo;
+using WhereToFly.Geo.Model;
+
+namespace WhereToFly.App.UnitTest.Geo
+{
+    /// <summary>
+    /// Unit tests for GeoJSON converter classes
+    /// </summary>
+    [TestClass]
+    public class CoordinatesParserTest
+    {
+        /// <summary>
+        /// Tests parsing all valid coordinates text strings
+        /// </summary>
+        /// <param name="text">coordinates text to parse</param>
+        [TestMethod]
+        [DataRow("49.781231, 6.837581")] // takeoff Mehring, Mosel
+        [DataRow("49.781231 6.837581")]
+        [DataRow("49.781231,6.837581")]
+        [DataRow("49.781231,   6.837581")]
+        [DataRow("+49.781231 +6.837581")]
+        [DataRow("+49.781231 -6.837581")]
+        [DataRow("-49.781231 +6.837581")]
+        [DataRow("-49.781231 -6.837581")]
+        [DataRow("49°46'52.4\"N 6°50'15.3\"E")]
+        [DataRow("49° 46′ 52.4″N 006° 50′ 15.3″E")]
+        [DataRow("49° 46′ N, 06° 50′ O ")]
+        [DataRow("49° 46′ S, 06° 50′ E")]
+        [DataRow("49° 46', -6° 50'")]
+        [DataRow("41°32'13\"N   109°32'59\"W")]
+        [DataRow("49° 34′ 45.59″ N, 12° 17′ 13.85″ E")]
+        [DataRow("49.579331°, 6.837581°")]
+        [DataRow("49.579331°N 6.837581°E")]
+        [DataRow("https://maps.google.com/maps?q=49.781231,6.837581")]
+        [DataRow("https://www.google.com/maps/place/47%C2%B038'32.8%22N+12%C2%B002'33.2%22E/@47.642442,12.0399761,17z")]
+        [DataRow("geo:49.781231,6.837581")]
+        public void TestParseValidCoordinates(string text)
+        {
+            // run
+            bool result = CoordinatesParser.TryParse(text, out MapPoint? mapPoint);
+
+            // check
+            Assert.IsTrue(result, "TryParse() must return true");
+            Assert.IsNotNull(mapPoint, "map point must not be null");
+            Assert.IsTrue(mapPoint.Valid, "map point must be valid");
+        }
+
+        /// <summary>
+        /// Tests parsing all invalid coordinates text strings
+        /// </summary>
+        /// <param name="text">coordinates text to parse</param>
+        [TestMethod]
+        [DataRow(null)]
+        [DataRow("")]
+        [DataRow(" ")]
+        [DataRow("\t\t\n\r\t")]
+        [DataRow("49,781231,6,837581")]
+        [DataRow("https://maps.google.com/maps?abc=49.781231,6.837581")]
+        [DataRow("geo:49.781231")]
+        public void TestParseInvalidCoordinatesText(string? text)
+        {
+            // run
+            bool result = CoordinatesParser.TryParse(text, out MapPoint? mapPoint);
+
+            // check
+            Assert.IsFalse(result, "TryParse() must return false");
+            Assert.IsNull(mapPoint, "map point must be null");
+        }
+    }
+}
---
+++ src/Geo/CoordinatesParser.cs
@@ -0,0 +1,406 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using WhereToFly.Geo.Model;
+
+namespace WhereToFly.Geo
+{
+    /// <summary>
+    /// Parser for a wide range of coordinate formats, mostly for WGS84.
+    /// </summary>
+    public static class CoordinatesParser
+    {
+        /// <summary>
+        /// Tries parsing a text string and finding latitude and longitude values.
+        /// </summary>
+        /// <param name="text">text to parse</param>
+        /// <param name="mapPoint">
+        /// map point, containing parsed values of latitude and longitude
+        /// </param>
+        /// <returns>true when parsing succeeded, or false when not</returns>
+        public static bool TryParse(string? text, out MapPoint? mapPoint)
+        {
+            mapPoint = null;
+
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return false;
+            }
+
+            text = text!
+                .Replace("\t", " ")
+                .Replace("\n", " ")
+                .Replace("\r", " ");
+
+            while (text.Contains("  "))
+            {
+                text = text.Replace("  ", " ");
+            }
+
+            text = text.Trim();
+
+            // try parse as URI
+            if (text.StartsWith("http://", ignoreCase: true, null) ||
+                text.StartsWith("https://", ignoreCase: true, null) ||
+                text.StartsWith("geo:", ignoreCase: true, null))
+            {
+                return TryParseUri(text, out mapPoint);
+            }
+
+            text = text
+                .Replace("′", "'")
+                .Replace("″", "\"")
+                .Replace("° ", "°")
+                .Replace("' ", "'")
+                .Replace("\" ", "\"");
+
+            // try to split the text into two parts
+            if (!TrySplitCoordinates(text, out string? latitude, out string? longitude) ||
+                latitude == null ||
+                longitude == null)
+            {
+                return false;
+            }
+
+            if (TryParseDecimals(latitude, longitude, out mapPoint))
+            {
+                return true;
+            }
+
+            if (TryParseDegreesMinutesSeconds(latitude, longitude, out mapPoint))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Tries to split text into latitude and longitude
+        /// </summary>
+        /// <param name="text">coordinates text to split</param>
+        /// <param name="latitude">latitude text</param>
+        /// <param name="longitude">longitude text</param>
+        /// <returns>true when splitting succeeded, or false when not</returns>
+        private static bool TrySplitCoordinates(string text, out string? latitude, out string? longitude)
+        {
+            latitude = null;
+            longitude = null;
+
+            int posCommaSpace = text.IndexOf(", ");
+            if (posCommaSpace != -1 &&
+                posCommaSpace == text.LastIndexOf(", "))
+            {
+                text = text.Replace(", ", ",");
+            }
+
+            int splitPos = -1;
+
+            int countSpaces = text.Count(c => c == ' ');
+            int countCommas = text.Count(c => c == ',');
+
+            if (countSpaces == 1)
+            {
+                splitPos = text.IndexOf(' ');
+            }
+            else if (countCommas == 1)
+            {
+                splitPos = text.IndexOf(',');
+            }
+
+            if (splitPos == -1)
+            {
+                return false;
+            }
+
+            latitude = text.Substring(0, splitPos);
+            longitude = text.Substring(splitPos + 1).TrimStart();
+
+            latitude = latitude.Replace(" ", string.Empty);
+            longitude = longitude.Replace(" ", string.Empty);
+
+            CheckSwapLatLong(ref latitude, ref longitude);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Compass direction characters for latitude
+        /// </summary>
+        private static readonly char[] LatitudeDirectionCharacter = new char[] { 'N', 'S' };
+
+        /// <summary>
+        /// Compass direction characters for longitude
+        /// </summary>
+        private static readonly char[] LongitudeDirectionCharacter = new char[] { 'E', 'W', 'O' };
+
+        /// <summary>
+        /// Check if the split latitude and longitude strings actually end with the direction
+        /// characters for the other compass direction and swaps them if necessary.
+        /// </summary>
+        /// <param name="latitude">latitude text to check</param>
+        /// <param name="longitude">longitude text to check</param>
+        private static void CheckSwapLatLong(ref string latitude, ref string longitude)
+        {
+            string localLatitude = latitude;
+            string localLongitude = longitude;
+
+            bool endsWithSwappedDirectionCharacter =
+                LongitudeDirectionCharacter.Any(c => localLatitude.EndsWith(c.ToString())) &&
+                LatitudeDirectionCharacter.Any(c => localLongitude.EndsWith(c.ToString()));
+
+            bool startsWithSwappedDirectionCharacter =
+                LongitudeDirectionCharacter.Any(c => localLatitude.StartsWith(c.ToString())) &&
+                LatitudeDirectionCharacter.Any(c => localLongitude.StartsWith(c.ToString()));
+
+            if (endsWithSwappedDirectionCharacter ||
+                startsWithSwappedDirectionCharacter)
+            {
+                string temp = latitude;
+                latitude = longitude;
+                longitude = temp;
+            }
+
+            latitude = latitude.Trim(LatitudeDirectionCharacter);
+            longitude = longitude.Trim(LongitudeDirectionCharacter);
+        }
+
+        /// <summary>
+        /// Tries parsing decimal coordinates
+        /// </summary>
+        /// <param name="latitude">latitude text</param>
+        /// <param name="longitude">longitude text</param>
+        /// <param name="mapPoint">
+        /// map point, containing parsed values of latitude and longitude
+        /// </param>
+        /// <returns>true when parsing succeeded, or false when not</returns>
+        private static bool TryParseDecimals(
+            string latitude,
+            string longitude,
+            out MapPoint? mapPoint)
+        {
+            mapPoint = null;
+            latitude = latitude.TrimEnd('°').TrimEnd();
+            longitude = longitude.TrimEnd('°').TrimEnd();
+
+            const NumberStyles numberStyles =
+                NumberStyles.AllowDecimalPoint |
+                NumberStyles.AllowLeadingSign;
+
+            if (!double.TryParse(
+                    latitude,
+                    numberStyles,
+                    CultureInfo.InvariantCulture,
+                    out double parsedLatitude) ||
+                !double.TryParse(
+                    longitude,
+                    numberStyles,
+                    CultureInfo.InvariantCulture,
+                    out double parsedLongitude))
+            {
+                return false;
+            }
+
+            mapPoint = new MapPoint(
+                latitude: parsedLatitude,
+                longitude: parsedLongitude);
+
+            return true;
+        }
+
+        private static bool TryParseDegreesMinutesSeconds(
+            string latitude,
+            string longitude,
+            out MapPoint? mapPoint)
+        {
+            if (!TryParseSingleDmsValue(latitude, out double parsedLatitude) ||
+                !TryParseSingleDmsValue(latitude, out double parsedLongitude))
+            {
+                mapPoint = null;
+                return false;
+            }
+            else
+            {
+                mapPoint = new MapPoint(
+                    latitude: parsedLatitude,
+                    longitude: parsedLongitude);
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Tries to parse a single degrees-minutes-seconds text, either latitude or longitude
+        /// </summary>
+        /// <param name="latitudeOrLongitudeText">
+        /// latitide or longitude text to parse
+        /// </param>
+        /// <param name="latitudeOrLongitudeValue">
+        /// parsed latitude or longitude value
+        /// </param>
+        /// <returns>true when parsing succeeded, or false when not</returns>
+        private static bool TryParseSingleDmsValue(
+            string latitudeOrLongitudeText,
+            out double latitudeOrLongitudeValue)
+        {
+            latitudeOrLongitudeValue = 0.0;
+
+            if (latitudeOrLongitudeText.Count(c => c == '°') > 1 ||
+                latitudeOrLongitudeText.Count(c => c == '\'') > 1 ||
+                latitudeOrLongitudeText.Count(c => c == '"') > 1)
+            {
+                return false;
+            }
+
+            if (!latitudeOrLongitudeText.Contains("°") ||
+                !latitudeOrLongitudeText.Contains("'") ||
+                latitudeOrLongitudeText.LastIndexOf("°") >= latitudeOrLongitudeText.LastIndexOf("'"))
+            {
+                return false;
+            }
+
+            if (latitudeOrLongitudeText.Contains("'") &&
+                latitudeOrLongitudeText.Contains("\"") &&
+                latitudeOrLongitudeText.LastIndexOf("\"") < latitudeOrLongitudeText.IndexOf("'"))
+            {
+                return false;
+            }
+
+            string[] splitValues = latitudeOrLongitudeText.Split('°', '\'');
+
+            // no seconds after minutes?
+            if (splitValues.Length == 3 &&
+                splitValues[2].Length == 0)
+            {
+                splitValues = splitValues.Take(2).ToArray();
+            }
+
+            if (splitValues.Length < 2 ||
+                splitValues.Length > 3 ||
+                splitValues.Any(value => value.Length == 0))
+            {
+                return false;
+            }
+
+            string degreesText = splitValues[0];
+            bool isNegative = degreesText.StartsWith("-");
+
+            if (isNegative)
+            {
+                degreesText = degreesText.TrimStart('-');
+            }
+
+            degreesText = degreesText.TrimStart('+');
+
+            if (!double.TryParse(
+                degreesText,
+                NumberStyles.AllowDecimalPoint,
+                CultureInfo.InvariantCulture,
+                out double degreesValue))
+            {
+                return false;
+            }
+
+            latitudeOrLongitudeValue = degreesValue;
+
+            if (!double.TryParse(
+                splitValues[1],
+                NumberStyles.AllowDecimalPoint,
+                CultureInfo.InvariantCulture,
+                out double minutesValue))
+            {
+                return false;
+            }
+
+            latitudeOrLongitudeValue += minutesValue / 60.0;
+
+            if (splitValues.Length == 3)
+            {
+                string secondsText = splitValues[2];
+                secondsText = secondsText.TrimEnd('"');
+
+                if (!double.TryParse(
+                    secondsText,
+                    NumberStyles.AllowDecimalPoint,
+                    CultureInfo.InvariantCulture,
+                    out double secondsValue))
+                {
+                    return false;
+                }
+
+                latitudeOrLongitudeValue += secondsValue / 3600.0;
+            }
+
+            if (isNegative)
+            {
+                latitudeOrLongitudeValue = -latitudeOrLongitudeValue;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Tries to parse an URI that may contain location or a placemark
+        /// </summary>
+        /// <param name="text">URI text</param>
+        /// <param name="mapPoint">
+        /// map point, containing parsed location coordinates
+        /// </param>
+        /// <returns>true when parsing succeeded, or false when not</returns>
+        private static bool TryParseUri(string text, out MapPoint? mapPoint)
+        {
+            mapPoint = null;
+
+            if (!Uri.TryCreate(text, UriKind.Absolute, out Uri uri))
+            {
+                return false;
+            }
+
+            var mapsHostnames = new string[]
+            {
+                "maps.google.com",
+                "www.google.com",
+            };
+
+            if (mapsHostnames.Contains(uri.Host.ToLowerInvariant()))
+            {
+                if (uri.LocalPath.StartsWith(
+                    "/maps/place/",
+                    StringComparison.OrdinalIgnoreCase))
+                {
+                    string[] parts = uri.LocalPath.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (parts.Length >= 2)
+                    {
+                        string placeText = parts[2].Replace("+", ",");
+                        return TryParse(placeText, out mapPoint);
+                    }
+                }
+
+                Dictionary<string, string> uriParameterMap =
+                    uri.Query.Split('&')
+                        .Select(parameters => parameters.Split('='))
+                        .Where(keyAndValue => keyAndValue.Length == 2)
+                        .ToDictionary(
+                            keyAndValue => keyAndValue[0].ToLowerInvariant().Trim('?', '&'),
+                            keyAndValue => keyAndValue[1]);
+
+                if (!uriParameterMap.TryGetValue("q", out string param))
+                {
+                    return false;
+                }
+                else
+                {
+                    return TryParse(param, out mapPoint);
+                }
+            }
+
+            // parse geo: URI
+            if (uri.Scheme.ToLowerInvariant() == "geo")
+            {
+                return TryParse(uri.PathAndQuery, out mapPoint);
+            }
+
+            return false;
+        }
+    }
+}
