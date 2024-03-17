25384ffc4e85f22e044c5abf6e23b8fa5a0a1e11
Michael Fink <michael.fink@asamnet.de>
2024-03-17 18:02:11 +0100

    changed running unit tests to use dotnet test and install ReportGenerator using dotnet tool install

--- src/RunUnitTests.cmd
+++ src/RunUnitTests.cmd
@@ -5,43 +5,32 @@ REM Copyright (C) 2017-2024 Michael Fink
 REM
 REM Runs Unit tests and coverage analysis
 REM
-
-REM set this to your Visual Studio installation folder
-set VSINSTALL=%ProgramFiles%\Microsoft Visual Studio\2022\Community
-
-REM set this to your OpenCover executable
-set OPENCOVER="C:\Projekte\Tools\OpenCover\OpenCover.Console.exe"
-
-REM set this to your ReportGenerator executable
-set REPORTGENERATOR="C:\Projekte\Tools\ReportGenerator\ReportGenerator.exe"
+echo RunUnitTests.cmd - Runs unit tests and generates coverage report
+echo.
 
 REM
 REM Preparations
 REM
-call "%VSINSTALL%\Common7\Tools\VsDevCmd.bat"
-
-mkdir "%~dp0\TestResults" 2> nul
 
 set DOTNET_CLI_TELEMETRY_OPTOUT=1
 
-set VSTEST=%VSINSTALL%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe
+REM install ReportGenerator, if not available yet
+dotnet tool install dotnet-reportgenerator-globaltool --tool-path .dotnet-tools
 
-REM
-REM Build projects
-REM
-dotnet publish App\UnitTest\WhereToFly.App.UnitTest.csproj --configuration Release
+set PATH=%PATH%;"%CD%\.dotnet-tools"
+
+mkdir "%~dp0\TestResults" 2> nul
 
 REM
 REM Run Unit-Tests
 REM
-%OPENCOVER% ^
-    -register:user ^
-    -target:"%VSTEST%" ^
-    -targetargs:"\"%~dp0App\UnitTest\bin\Release\net48\WhereToFly.App.UnitTest.dll\"" ^
-    -filter:"+[WhereToFly*]* -[WhereToFly.App.Android]* -[WhereToFly.App.UnitTest]*" ^
-    -mergebyhash ^
-    -skipautoprops ^
-    -output:"%~dp0\TestResults\WhereToFly-App-CoverageReport.xml"
+dotnet test ^
+    App\UnitTest\WhereToFly.App.UnitTest.csproj ^
+    --configuration Release ^
+    --collect:"XPlat Code Coverage;Format=opencover" ^
+    --results-directory TestResults ^
+    --logger:trx;LogFileName=WhereToFly-App-Log.xml ^
+    --logger:console;verbosity=detailed
 
 dotnet test ^
     Geo\UnitTest\WhereToFly.Geo.UnitTest.csproj ^
@@ -59,8 +48,8 @@ dotnet test ^
     --logger:trx;LogFileName=WhereToFly-WebApi-Log.trx ^
     --logger:console;verbosity=detailed
 
-%REPORTGENERATOR% ^
-    -reports:TestResults\WhereToFly-*-CoverageReport.xml;TestResults\**\*.opencover.xml ^
+ReportGenerator ^
+    -reports:TestResults\**\*.opencover.xml ^
     -targetdir:"%~dp0\TestResults\CoverageReport"
 
 pause
