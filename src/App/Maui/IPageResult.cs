8ed4775669e359bb9711867a66aafe1789bf0676
Michael Fink <michael.fink@asamnet.de>
2024-04-21 22:12:24 +0200

    implemented BasePopupPage for Maui app

---
+++ src/App/Maui/IPageResult.cs
@@ -0,0 +1,16 @@
+﻿namespace WhereToFly.App
+{
+    /// <summary>
+    /// Interface for pages and popup pages that wish to return a result
+    /// </summary>
+    /// <typeparam name="TResult">type of result to return</typeparam>
+    public interface IPageResult<TResult>
+        where TResult : class?
+    {
+        /// <summary>
+        /// Returns task that returns a result when awaited; when the page is cancelled, may
+        /// return null.
+        /// </summary>
+        Task<TResult?> ResultTask { get; }
+    }
+}
---
+++ src/App/Maui/Views/BasePopupPageOfT.cs
@@ -0,0 +1,57 @@
+﻿namespace WhereToFly.App.Views
+{
+    /// <summary>
+    /// Base class for all popup pages that want to return a result.
+    /// </summary>
+    /// <typeparam name="TResult">type of the result</typeparam>
+    public class BasePopupPage<TResult> : BasePopupPage, IPageResult<TResult>
+        where TResult : class?
+    {
+        /// <summary>
+        /// Task that resolves to the result object when the popup is closed.
+        /// </summary>
+        public Task<TResult?> ResultTask => this.GetResult();
+
+        /// <summary>
+        /// Creates a new base popup page
+        /// </summary>
+        public BasePopupPage()
+        {
+            this.CanBeDismissedByTappingOutsideOfPopup = true;
+        }
+
+        /// <summary>
+        /// Sets result value for popup page and closes the popup
+        /// </summary>
+        /// <param name="result">result value</param>
+        protected void SetResult(TResult result)
+        {
+            this.Close(result);
+        }
+
+        /// <summary>
+        /// Retrieves the result of the popup page
+        /// </summary>
+        /// <returns>result object, or null when user cancelled the popup</returns>
+        /// <exception cref="InvalidOperationException">
+        /// thrown when a different type of object was returned as result
+        /// </exception>
+        private async Task<TResult?> GetResult()
+        {
+            object? result = await this.Result;
+            if (result == null)
+            {
+                return null;
+            }
+
+            if (result is not TResult)
+            {
+                throw new InvalidOperationException(
+                    $"Result task is not of type {typeof(TResult).FullName} " +
+                    $"but instead of type {result.GetType().FullName}");
+            }
+
+            return result as TResult;
+        }
+    }
+}
