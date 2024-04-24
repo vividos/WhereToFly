599b2a678c345066df0f9bab371fb90cf8dba98a
Michael Fink <michael.fink@asamnet.de>
2024-04-24 21:17:02 +0200

    added MapView to Maui MainPage for tests

--- src/App/Maui/MainPage.xaml.cs
+++ src/App/Maui/MainPage.xaml.cs
@@ -11,6 +11,15 @@
         public MainPage()
         {
             this.InitializeComponent();
+
+            this.Dispatcher.DispatchAsync(
+                async () =>
+                {
+                    await this.mapView.CreateAsync(
+                        new Geo.Model.MapPoint(47.67, 11.88),
+                        5000,
+                        false);
+                });
         }
     }
 }
