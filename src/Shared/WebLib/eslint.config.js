7192ed51ade9be85ad79cddec87ccde9e307e504
Michael Fink <michael.fink@asamnet.de>
2024-04-27 11:15:04 +0200

    also added eslint-plugin-jsdoc plugin and recommended rules

--- src/Shared/WebLib/eslint.config.js
+++ src/Shared/WebLib/eslint.config.js
@@ -1,14 +1,19 @@
 // eslint flat config
 import js from "@eslint/js";
+import jsdoc from "eslint-plugin-jsdoc";
 import globals from "globals";
 
 export default [
     js.configs.recommended,
+    jsdoc.configs["flat/recommended"],
     {
         languageOptions: {
             globals: {
                 ...globals.browser
             }
+        },
+        plugins: {
+            jsdoc,
         },
         rules: {
             "indent": [ "error", 4 ],
