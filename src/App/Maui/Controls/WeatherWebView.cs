e89982eec0cff07e742af831606cbaa92e49243d
Michael Fink <michael.fink@asamnet.de>
2024-06-01 08:16:31 +0200

    moved Core files to Maui project; no code changes

---
+++ src/App/Maui/Controls/WeatherWebView.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+#pragma warning disable S2094 // Classes should not be empty
+
+namespace WhereToFly.App.Controls
+{
+    /// <summary>
+    /// Web view that shows the selected weather page. The web view's platform dependent renderer
+    /// allows to long-tap any image and save it.
+    /// </summary>
+    public class WeatherWebView : WebView
+    {
+    }
+}
