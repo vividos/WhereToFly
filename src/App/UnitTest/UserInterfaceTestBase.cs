13819f6b57d84ddc6a5b8603f141adb9969f9126
Michael Fink <michael.fink@asamnet.de>
2024-06-21 08:10:12 +0200

    added mocked IFontNamedSizeService implementation to unit tests

--- src/App/UnitTest/UserInterfaceTestBase.cs
+++ src/App/UnitTest/UserInterfaceTestBase.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Maui;
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Internals;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using WhereToFly.App.Logic;
 using WhereToFly.App.Models;
 
@@ -11,6 +13,20 @@ namespace WhereToFly.App.UnitTest
     /// </summary>
     public class UserInterfaceTestBase
     {
+#pragma warning disable CS0612 // Type or member is obsolete
+        /// <summary>
+        /// Mock implementation of IFontNamedSizeService
+        /// </summary>
+        internal class MockFontNamedSizeService : IFontNamedSizeService
+        {
+            /// <inheritdoc />
+            public double GetNamedSize(NamedSize size, Type targetElementType, bool useOldSizes)
+            {
+                return 42.0;
+            }
+        }
+#pragma warning restore CS0612 // Type or member is obsolete
+
         /// <summary>
         /// Sets up unit test for testing user interface
         /// </summary>
@@ -19,11 +35,24 @@ namespace WhereToFly.App.UnitTest
         {
             MauiMocks.Init();
 
+#pragma warning disable CS0612 // Type or member is obsolete
+            DependencyService.Register<IFontNamedSizeService, MockFontNamedSizeService>();
+#pragma warning restore CS0612 // Type or member is obsolete
+
             DependencyService.Register<IPlatform, UnitTestPlatform>();
             DependencyService.Register<IGeolocationService, UnitTestGeolocationService>();
             DependencyService.Register<SvgImageCache>();
 
             App.Settings = new AppSettings();
+        }
+
+        /// <summary>
+        /// Cleans up unit test for testing user interface
+        /// </summary>
+        [TestCleanup]
+        public void TearDownUnitTestUserInterface()
+        {
+            MauiMocks.Reset();
         }
     }
 }
