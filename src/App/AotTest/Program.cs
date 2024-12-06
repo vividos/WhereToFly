284a03f9ff001750eb6f1923833999975e205edc
Michael Fink <michael.fink@asamnet.de>
2024-12-06 08:26:32 +0100

    converted AOT test code to full Program class

--- src/App/AotTest/Program.cs
+++ src/App/AotTest/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace WhereToFly.App.AotTest;
+
+/// <summary>
+/// AOT test program
+/// </summary>
+internal static class Program
+{
+    /// <summary>
+    /// Main entry point
+    /// </summary>
+    /// <param name="args">command line arguments</param>
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
