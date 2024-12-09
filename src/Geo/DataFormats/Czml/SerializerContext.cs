acab0ca21e4e7ab10c00d9dfd92ccc50499bcaa7
Michael Fink <michael.fink@asamnet.de>
2024-12-09 07:17:05 +0100

    fixed typo

--- src/Geo/DataFormats/Czml/SerializerContext.cs
+++ src/Geo/DataFormats/Czml/SerializerContext.cs
@@ -4,7 +4,7 @@ using System.Text.Json.Serialization;
 namespace WhereToFly.Geo.DataFormats.Czml
 {
     /// <summary>
-    /// Serializer contet for CZML object model
+    /// Serializer context for CZML object model
     /// </summary>
     [JsonSourceGenerationOptions(
         WriteIndented = false,
