d82a8b6de06e20eb20ebefa5fd0e2a003824d699
Michael Fink <michael.fink@asamnet.de>
2025-01-09 08:03:21 +0100

    added sample app for SvgImage

---
+++ src/App/Svg/Sample/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace WhereToFly.App.Svg.Sample
+{
+    /// <summary>
+    /// Sample app
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Creates a new app object
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Creates a new window
+        /// </summary>
+        /// <param name="activationState">activation state</param>
+        /// <returns>newly created window</returns>
+        protected override Window CreateWindow(
+            IActivationState? activationState)
+        {
+            return new Window(new SamplePage())
+            {
+                Title = "SvgImage sample app",
+            };
+        }
+    }
+}
