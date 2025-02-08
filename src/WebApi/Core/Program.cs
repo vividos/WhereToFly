a525962249664574c712648b36482ee2e165d0aa
Michael Fink <michael.fink@asamnet.de>
2025-02-08 20:33:58 +0100

    updated WebApi project to use top-level statements
    also use Microsoft.AspNetCore.OpenApi

--- src/WebApi/Core/Program.cs
+++ src/WebApi/Core/Program.cs
@@ -1,33 +1,60 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using WhereToFly.Shared.Model.Serializers;
+using WhereToFly.WebApi.Core.Services;
+using WhereToFly.WebApi.Logic;
+using WhereToFly.WebApi.Logic.Services;
 
-namespace WhereToFly.WebApi.Core
-{
-    /// <summary>
-    /// Main program for WebAPI
-    /// </summary>
-    public static class Program
+const string DebugCorsPolicyName = "DebugCorsPolicy";
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// Add services to the container
+builder.Services.AddSingleton<GarminInreachDataService>();
+builder.Services.AddSingleton<LiveWaypointCacheManager>();
+builder.Services.AddSingleton<LiveTrackCacheManager>();
+builder.Services.AddSingleton<LocationFindManager>();
+builder.Services.AddSingleton<PlanTourService>();
+
+builder.Services.AddOpenApi();
+
+builder.Services.ConfigureHttpJsonOptions(
+    options =>
     {
-        /// <summary>
-        /// Main entry point for WebAPI
-        /// </summary>
-        /// <param name="args">command line args</param>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage(
-            "Sonar Code Smell",
-            "S4823:Using command line arguments is security-sensitive",
-            Justification = "ASP.NET Core boilerplate code")]
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+        options.SerializerOptions.TypeInfoResolverChain.Add(
+            SharedModelJsonSerializerContext.Default);
+    });
 
-        /// <summary>
-        /// Returns a web host builder object
-        /// </summary>
-        /// <param name="args">command line args</param>
-        /// <returns>built web host object</returns>
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-    }
+builder.Services.AddCors(
+    options =>
+    {
+        options.AddPolicy(
+            DebugCorsPolicyName,
+            builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+    });
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseCors(DebugCorsPolicyName);
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint(
+            "/openapi/v1.json",
+            "WhereToFly Web API v1");
+    });
 }
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+await app.RunAsync();
