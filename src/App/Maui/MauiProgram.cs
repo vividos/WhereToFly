9bd03f8676310492c6b1d1c6ca45de24e6040a37
Michael Fink <michael.fink@asamnet.de>
2025-03-25 07:45:40 +0100

    disabled warning CA1416 from CommunityToolkit.Maui when compiling for net9.0

--- src/App/Maui/MauiProgram.cs
+++ src/App/Maui/MauiProgram.cs
@@ -18,6 +18,8 @@ namespace WhereToFly.App
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
+
+#pragma warning disable CA1416 // Validate platform compatibility
             builder
                 .UseMauiApp<App>()
 #if WINDOWS
@@ -37,6 +39,7 @@ namespace WhereToFly.App
 #endif
                 .UseSkiaSharp()
                 .UseMapView();
+#pragma warning restore CA1416 // Validate platform compatibility
 
             builder = WeatherWebView.UseWeatherWebView(builder);
 
