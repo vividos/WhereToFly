9466a31d818a0107ac2e6e5e928f4e75bc944cd8
Michael Fink <michael.fink@asamnet.de>
2025-04-10 19:35:12 +0200

    updated version number to 1.17.0

--- src/SonarCloud.cmd
+++ src/SonarCloud.cmd
@@ -1,7 +1,7 @@
 @echo off
 REM
 REM Where-to-fly - an app to decide where to (hike up and) fly with a paraglider
-REM Copyright (C) 2017-2024 Michael Fink
+REM Copyright (C) 2017-2025 Michael Fink
 REM
 REM Runs SonarCloud analysis build
 REM
@@ -27,7 +27,7 @@ rmdir .\bw-output /s /q 2> nul
 
 dotnet-sonarscanner begin ^
     /k:"WhereToFly" ^
-    /v:"1.16.0" ^
+    /v:"1.17.0" ^
     /d:"sonar.cs.opencover.reportsPaths=%CD%\TestResults\**\*.opencover.xml" ^
     /d:"sonar.exclusions=Web\LiveTracking\wwwroot\lib\**\*" ^
     /d:"sonar.host.url=https://sonarcloud.io" ^
