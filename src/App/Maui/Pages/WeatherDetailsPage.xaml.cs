5bc909ef0543d1d639f11384fd6f785a163488b4
Michael Fink <michael.fink@asamnet.de>
2025-10-07 18:34:56 +0200

    refactor: split WeatherDetailsPage in xaml, page and view model

---
+++ src/App/Maui/Pages/WeatherDetailsPage.xaml.cs
@@ -0,0 +1,134 @@
+﻿using WhereToFly.App.Behaviors;
+using WhereToFly.App.Models;
+using WhereToFly.App.ViewModels;
+
+namespace WhereToFly.App.Pages
+{
+    /// <summary>
+    /// Page showing weather details by showing an external web page. Also the page has quick
+    /// select menus for all available weather pages shown on the dashboard.
+    /// </summary>
+    public partial class WeatherDetailsPage : ContentPage
+    {
+        /// <summary>
+        /// Creates new weather details page
+        /// </summary>
+        /// <param name="iconDescription">weather icon description to open</param>
+        public WeatherDetailsPage(WeatherIconDescription iconDescription)
+        {
+            this.InitializeComponent();
+
+            var viewModel = new WeatherDetailsViewModel(this.weatherWebView);
+
+            this.BindingContext = viewModel;
+
+            if (this.weatherWebView.Behaviors.Count == 0)
+            {
+#if ANROID || WINDOWS
+                this.weatherWebView.Behaviors.Add(
+                    new WebViewLongTapToSaveImageBehavior());
+#endif
+
+                this.weatherWebView.Navigated +=
+                    async (sender, args) =>
+                    await OnNavigated_WebView(sender, args);
+            }
+
+            viewModel.OpenWebLink(iconDescription);
+        }
+
+        /// <summary>
+        /// Called when navigation to current page has finished
+        /// </summary>
+        /// <param name="sender">sender object</param>
+        /// <param name="args">event args</param>
+        /// <returns>task to wait on</returns>
+        private static async Task OnNavigated_WebView(
+            object? sender,
+            WebNavigatedEventArgs args)
+        {
+            if (sender is WebView webView &&
+                args.Url.StartsWith("https://www.austrocontrol.at/flugwetter/"))
+            {
+                await CheckAlpthermLogin(webView);
+            }
+        }
+
+        /// <summary>
+        /// Checks if login for alptherm is needed on the current web view.
+        /// </summary>
+        /// <param name="webView">web view to use</param>
+        /// <returns>task to wait on</returns>
+        private static async Task CheckAlpthermLogin(IWebView webView)
+        {
+            try
+            {
+                string result = await webView.EvaluateJavaScriptAsync(
+                    "javascript:document.getElementById('login-form') !== null;");
+
+                if (result == "true")
+                {
+                    await PasteAlpthermUsernamePassword(webView);
+                }
+            }
+            catch (Exception)
+            {
+                // ignore errors from check
+            }
+        }
+
+        /// <summary>
+        /// Inserts alptherm username and password on the current web view.
+        /// </summary>
+        /// <param name="webView">web view to use</param>
+        /// <returns>task to wait on</returns>
+        private static async Task PasteAlpthermUsernamePassword(IWebView webView)
+        {
+            string? username = await SecureStorage.GetAsync(Constants.SecureSettingsAlpthermUsername);
+            string? password = await SecureStorage.GetAsync(Constants.SecureSettingsAlpthermPassword);
+
+            if (string.IsNullOrEmpty(username) &&
+                string.IsNullOrEmpty(password))
+            {
+                return;
+            }
+
+            string js = $"javascript:";
+
+            if (!string.IsNullOrEmpty(username))
+            {
+                js += $"document.getElementById('httpd_username').value = '{username}';";
+            }
+
+            if (!string.IsNullOrEmpty(password))
+            {
+                js += $"document.getElementById('httpd_password').value = '{password}';";
+            }
+
+            if (!string.IsNullOrEmpty(username) &&
+                !string.IsNullOrEmpty(password))
+            {
+                js += "document.getElementById('httpd_username').form.submit();";
+            }
+
+            await webView.EvaluateJavaScriptAsync(js);
+        }
+
+        /// <summary>
+        /// Called when the hardware back button is pressed; navigates back in browser, if
+        /// possible, or lets Forms handle the navigation.
+        /// </summary>
+        /// <returns>true when the back button press was handled, false when not</returns>
+        protected override bool OnBackButtonPressed()
+        {
+            if (this.Content is WebView webView &&
+                webView.CanGoBack)
+            {
+                webView.GoBack();
+                return true;
+            }
+
+            return base.OnBackButtonPressed();
+        }
+    }
+}
---
+++ src/App/Maui/ViewModels/WeatherDetailsViewModel.cs
@@ -0,0 +1,106 @@
+﻿using System.Windows.Input;
+using WhereToFly.App.Models;
+
+namespace WhereToFly.App.ViewModels;
+
+/// <summary>
+/// View model for the weather details page
+/// </summary>
+internal class WeatherDetailsViewModel : ViewModelBase
+{
+    /// <summary>
+    /// Weather web view
+    /// </summary>
+    private readonly IWebView weatherWebView;
+
+    #region Binding properties
+
+    /// <summary>
+    /// Title text
+    /// </summary>
+    public string Title { get; private set; } = string.Empty;
+
+    /// <summary>
+    /// Web view source for the weather web view
+    /// </summary>
+    public WebViewSource? WeatherWebViewSource { get; private set; }
+
+    /// <summary>
+    /// Refresh toolbar command
+    /// </summary>
+    public ICommand RefreshCommand { get; }
+
+    /// <summary>
+    /// Weather forecast toolbar command
+    /// </summary>
+    public ICommand WeatherForecastCommand { get; }
+
+    /// <summary>
+    /// Current weather toolbar command
+    /// </summary>
+    public ICommand CurrentWeatherCommand { get; }
+
+    /// <summary>
+    /// Webcams toolbar command
+    /// </summary>
+    public ICommand WebcamsCommand { get; }
+    #endregion
+
+    /// <summary>
+    /// Creates a new view model
+    /// </summary>
+    /// <param name="weatherWebView">weather web view implementation</param>
+    public WeatherDetailsViewModel(IWebView weatherWebView)
+    {
+        this.weatherWebView = weatherWebView;
+
+        this.RefreshCommand = new Command(
+            () => this.weatherWebView.Reload());
+
+        this.WeatherForecastCommand = new AsyncRelayCommand(
+            async () => await this.SelectAndOpenWeatherPageAsync("Weather forecast"));
+
+        this.CurrentWeatherCommand = new AsyncRelayCommand(
+            async () => await this.SelectAndOpenWeatherPageAsync("Current weather"));
+
+        this.WebcamsCommand = new AsyncRelayCommand(
+            async () => await this.SelectAndOpenWeatherPageAsync("Webcams"));
+    }
+
+    /// <summary>
+    /// (Re-)opens web link from weather icon description
+    /// </summary>
+    /// <param name="iconDescription">weather icon description to open</param>
+    public void OpenWebLink(WeatherIconDescription iconDescription)
+    {
+        this.Title = iconDescription.Name;
+        this.OnPropertyChanged(nameof(this.Title));
+
+        this.WeatherWebViewSource = new UrlWebViewSource
+        {
+            Url = iconDescription.WebLink,
+        };
+
+        this.OnPropertyChanged(nameof(this.WeatherWebViewSource));
+    }
+
+    /// <summary>
+    /// Lets the userselect a different weather icon description from given group and opens
+    /// the new web link in the current web view.
+    /// </summary>
+    /// <param name="group">group to filter weather icon descriptions by</param>
+    /// <returns>task to wait on</returns>
+    private async Task SelectAndOpenWeatherPageAsync(string group)
+    {
+        var weatherIcon =
+            await UserInterface.NavigationService.NavigateToPopupPageAsync<WeatherIconDescription>(
+                PopupPageKey.SelectWeatherIconPopupPage,
+                animated: true,
+                group);
+
+        if (weatherIcon != null)
+        {
+            this.OpenWebLink(weatherIcon);
+        }
+    }
+}
